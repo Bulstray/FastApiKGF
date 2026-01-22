183abc56c0343e6848128f6c3925638d269f14e6
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-01-22 13:45:13 +0300

    add settings for id

--- fastapi-kgf/core/models/base.py
+++ fastapi-kgf/core/models/base.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped
+from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
 
 from utils import camel_case_to_snake_case
 
@@ -6,7 +6,10 @@ from utils import camel_case_to_snake_case
 class Base(DeclarativeBase):
     __abstract__ = True
 
-    id: Mapped[int]
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+        autoincrement=True,
+    )
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
