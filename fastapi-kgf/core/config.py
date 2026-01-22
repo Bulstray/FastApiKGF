78c687e8037477da4d7266300946f00f48d0f4ad
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-01-22 14:34:41 +0300

    add api prefix config

--- fastapi-kgf/core/config.py
+++ fastapi-kgf/core/config.py
@@ -2,6 +2,16 @@ from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 
 
+class ApiV1Prefix(BaseModel):
+    prefix: str = "/v1"
+    programs: str = "/programs"
+
+
+class ApiPrefix(BaseModel):
+    prefix: str = "/api"
+    v1: ApiV1Prefix = ApiV1Prefix()
+
+
 class DatabaseConfig(BaseModel):
     url: str = "sqlite:///database.db"
     echo: bool = False
@@ -9,6 +19,7 @@ class DatabaseConfig(BaseModel):
 
 class Settings(BaseSettings):
     db: DatabaseConfig = DatabaseConfig()
+    api: ApiPrefix = ApiPrefix()
 
 
 settings = Settings()
