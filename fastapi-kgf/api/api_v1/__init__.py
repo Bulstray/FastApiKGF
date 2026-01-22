da8f425101a08b480464c52fb5c24d5b4f4b1679
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-01-22 14:44:48 +0300

    include programs router

--- fastapi-kgf/api/api_v1/__init__.py
+++ fastapi-kgf/api/api_v1/__init__.py
@@ -2,6 +2,12 @@ from fastapi import APIRouter
 
 from core.config import settings
 
+from .programs import router as programs_router
+
 router = APIRouter(
     prefix=settings.api.v1.prefix,
+)
+
+programs_router.include_router(
+    prefix=settings.api.v1.programs,
 )
