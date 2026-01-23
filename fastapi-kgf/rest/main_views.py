4aca89bdb6ba5e2fb835932f960e48d4ccb81c00
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-01-23 12:15:59 +0300

    fix annotation

--- fastapi-kgf/rest/main_views.py
+++ fastapi-kgf/rest/main_views.py
@@ -1,13 +1,12 @@
+from typing import Annotated, Any
+
 from fastapi import APIRouter, Depends
 from fastapi.requests import Request
 from fastapi.responses import HTMLResponse
-
-from crud import programs as crud_programs
-
-from typing import Annotated
 from sqlalchemy.orm import Session
-from core.models import db_helper
 
+from core.models import db_helper
+from crud import programs as crud_programs
 from templating.jinja_template import templates
 
 router = APIRouter(include_in_schema=False)
@@ -21,7 +20,7 @@ def home_page(
         Depends(db_helper.session_getter),
     ],
 ) -> HTMLResponse:
-    context = {}
+    context: dict[str, Any] = {}
     programs = crud_programs.get_all_programs(
         session=session,
     )
