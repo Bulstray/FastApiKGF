60d7e7389f197bf40e1fdc293b1a629fddb32c61
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-01-23 11:09:12 +0300

    endpoint home

---
+++ fastapi-kgf/rest/main_views.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, Depends
+from fastapi.requests import Request
+from fastapi.responses import HTMLResponse
+
+from crud import programs as crud_programs
+
+from typing import Annotated
+from sqlalchemy.orm import Session
+from core.models import db_helper
+
+from templating.jinja_template import templates
+
+router = APIRouter(include_in_schema=False)
+
+
+@router.get("/", name="home")
+def home_page(
+    request: Request,
+    session: Annotated[
+        Session,
+        Depends(db_helper.session_getter),
+    ],
+) -> HTMLResponse:
+    context = {}
+    programs = crud_programs.get_all_programs(
+        session=session,
+    )
+
+    context.update(
+        programs=programs,
+    )
+
+    return templates.TemplateResponse(
+        request=request,
+        name="home.html",
+        context=context,
+    )
