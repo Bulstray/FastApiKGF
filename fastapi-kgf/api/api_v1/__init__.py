13f2c89ea671a3e8bbc2d7dee0a1f181d6a8cda1
Ramil Khakimov <bulstray@yandex.ru>
2026-01-24 17:20:45 +0300

    include new router

--- fastapi-kgf/api/api_v1/__init__.py
+++ fastapi-kgf/api/api_v1/__init__.py
@@ -3,10 +3,16 @@ from fastapi import APIRouter
 from core.config import settings
 
 from .programs import router as program_router
+from .users import router as users_router
 
 router = APIRouter(prefix=settings.api.v1.prefix)
 
 router.include_router(
     program_router,
     prefix=settings.api.v1.programs,
+)
+
+router.include_router(
+    users_router,
+    prefix=settings.api.v1.users,
 )
