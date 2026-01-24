67965507eb6095305678d57feb07d3d5213047b3
Ramil Khakimov <bulstray@yandex.ru>
2026-01-24 14:23:38 +0300

    fast import hash_password_simple

--- fastapi-kgf/utils/__init__.py
+++ fastapi-kgf/utils/__init__.py
@@ -1,4 +1,9 @@
-__all__ = ("camel_case_to_snake_case", "get_file_size")
+__all__ = (
+    "camel_case_to_snake_case",
+    "get_file_size",
+    "hash_password_simple",
+)
 
 from .case_converter import camel_case_to_snake_case
 from .file_size import get_file_size
+from .hash_password import hash_password_simple
