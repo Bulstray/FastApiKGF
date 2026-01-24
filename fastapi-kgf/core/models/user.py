b427012b133550b40254f80373cdd6300d87cee3
Ramil Khakimov <bulstray@yandex.ru>
2026-01-24 14:24:28 +0300

    remove class Enum

--- fastapi-kgf/core/models/user.py
+++ fastapi-kgf/core/models/user.py
@@ -1,17 +1,9 @@
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Enum
 
 from .base import Base
-
-import enum
-
-class UserRole(enum.Enum):
-    ADMIN = "admin"
-    USER = "user"
-    moderator = "moderator"
 
 
 class User(Base):
     user: Mapped[str]
     password: Mapped[str]
-    role: Mapped[str] = mapped_column(Enum(UserRole), default=UserRole.USER,)
+    role: Mapped[str]
