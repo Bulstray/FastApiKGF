64eb09a6bd335e7de7b79063b440a9766cc71d77
Ramil Khakimov <bulstray@yandex.ru>
2026-01-24 17:21:18 +0300

    fast import schemas user

--- fastapi-kgf/core/schemas/__init__.py
+++ fastapi-kgf/core/schemas/__init__.py
@@ -2,6 +2,10 @@ __all__ = (
     "Program",
     "ProgramCreate",
     "ProgramRead",
+    "User",
+    "UserRead",
+    "UserCreate",
 )
 
 from .programs import Program, ProgramCreate, ProgramRead
+from .user import User, UserRead, UserCreate
