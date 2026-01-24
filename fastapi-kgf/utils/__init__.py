4b1a291f9c72e3f362bd190ec75443848eabcd02
Ramil Khakimov <bulstray@yandex.ru>
2026-01-24 17:22:21 +0300

    update import utils package

--- fastapi-kgf/utils/__init__.py
+++ fastapi-kgf/utils/__init__.py
@@ -1,9 +1,7 @@
 __all__ = (
     "camel_case_to_snake_case",
     "get_file_size",
-    "hash_password_simple",
 )
 
 from .case_converter import camel_case_to_snake_case
 from .file_size import get_file_size
-from .hash_password import hash_password_simple
