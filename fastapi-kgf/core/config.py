86f876e953dfbfd3d7bb4b7d239b357b21638033
Ramil Khakimov <bulstray@yandex.ru>
2026-01-24 22:44:57 +0300

    add new class admin

--- fastapi-kgf/core/config.py
+++ fastapi-kgf/core/config.py
@@ -1,10 +1,16 @@
 from pathlib import Path
 
 from pydantic import BaseModel
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 BASE_UPLOADS_PROGRAMS: Path = Path("uploads/programs")
 BASE_UPLOADS_PROGRAMS.mkdir(parents=True, exist_ok=True)
+
+
+class Admin(BaseModel):
+    username: str
+    password: str
+    role: str
 
 
 class ApiV1Prefix(BaseModel):
@@ -24,8 +30,15 @@ class DatabaseConfig(BaseModel):
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=("env.template", ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
     db: DatabaseConfig = DatabaseConfig()
     api: ApiPrefix = ApiPrefix()
+    admin: Admin
 
 
 settings = Settings()
