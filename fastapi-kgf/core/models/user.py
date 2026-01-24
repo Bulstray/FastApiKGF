95a8a7e1a40e862b5dabe53a356b35277a8ef65f
Ramil Khakimov <bulstray@yandex.ru>
2026-01-24 17:23:24 +0300

    hashed password

--- fastapi-kgf/core/models/user.py
+++ fastapi-kgf/core/models/user.py
@@ -1,9 +1,20 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, validates, mapped_column
+
+import bcrypt
 
 from .base import Base
+from ..enums import UserRole
 
 
 class User(Base):
-    user: Mapped[str]
+    username: Mapped[str]
     password: Mapped[str]
-    role: Mapped[str]
+    role: Mapped[str] = mapped_column(default=UserRole.USER)
+
+    @validates("password")
+    def hash_password(self, key, password):
+        if password and not password.startswith("$2b$"):
+            password_bytes = password.encode("utf-8")
+            salt = bcrypt.gensalt()
+            return bcrypt.hashpw(password_bytes, salt).decode("utf-8")
+        return password
