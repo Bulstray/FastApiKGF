5bd2974af31bdb560283ce15723055ed2fcdacd7
Ramil Khakimov <bulstray@yandex.ru>
2026-01-25 18:04:34 +0300

    dependencies auth

---
+++ fastapi-kgf/services/auth/auth_helper.py
@@ -0,0 +1,19 @@
+import bcrypt
+from sqlalchemy.orm import Session
+
+from crud import user as user_crud
+
+
+def get_user_password(session: Session, username: str):
+    return user_crud.get_user_by_username(
+        session=session,
+        username=username,
+    )
+
+
+def check_password_match(password1: str, password2: str) -> bool:
+    hashed_password_bytes = password1.encode("utf-8")
+    return bcrypt.checkpw(
+        password=password2.encode("utf-8"),
+        hashed_password=hashed_password_bytes,
+    )
