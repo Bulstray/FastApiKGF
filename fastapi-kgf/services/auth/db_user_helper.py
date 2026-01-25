671cc0ca104eab60fa5b2c09724ef5573e247ed4
Ramil Khakimov <bulstray@yandex.ru>
2026-01-25 18:05:08 +0300

    auth user helper

---
+++ fastapi-kgf/services/auth/db_user_helper.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import Session
+
+from services.auth.auth_helper import get_user_password, check_password_match
+
+
+def validate_user_password(
+    session: Session,
+    username: str,
+    password: str,
+) -> bool:
+
+    db_password = get_user_password(
+        username=username,
+        session=session,
+    )
+
+    if db_password is None:
+        return False
+
+    return check_password_match(
+        password1=db_password[0].password,
+        password2=password,
+    )
