a8501ae3558151e998f6fa02203c30e52ce52429
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-01-28 09:38:32 +0300

    update annotation

--- fastapi-kgf/services/auth/auth_helper.py
+++ fastapi-kgf/services/auth/auth_helper.py
@@ -1,17 +1,18 @@
 import bcrypt
 from sqlalchemy.orm import Session
 
+from core.models import User
 from crud import user as user_crud
 
 
-def get_user_password(session: Session, username: str):
+def get_user_password(session: Session, username: str) -> tuple[User]:
     return user_crud.get_user_by_username(
         session=session,
         username=username,
     )
 
 
-def check_password_match(password1: str, password2: str) -> bool:
+def check_password_match(password1: bytes, password2: str) -> bool:
     hashed_password_bytes = password1
     return bcrypt.checkpw(
         password=password2.encode("utf-8"),
