0526960de0fefed06eecef08318895ffc1b4cb8f
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-01-29 13:23:00 +0300

    add settings for attr

--- fastapi-kgf/core/models/user.py
+++ fastapi-kgf/core/models/user.py
@@ -1,5 +1,5 @@
-import bcrypt
-from sqlalchemy.orm import Mapped, mapped_column, validates
+from sqlalchemy import Enum, String
+from sqlalchemy.orm import Mapped, mapped_column
 
 from core.enums import UserRole
 
@@ -7,14 +7,17 @@ from .base import Base
 
 
 class User(Base):
-    username: Mapped[str]
-    password: Mapped[str]
-    role: Mapped[str] = mapped_column(default=UserRole.USER)
+    __tablename__ = "user"
 
-    @validates("password")
-    def hash_password(self, key: str, password: str) -> bytes | str:
-        if password and not password.startswith("$2b$"):
-            password_bytes = password.encode("utf-8")
-            salt = bcrypt.gensalt()
-            return bcrypt.hashpw(password_bytes, salt)
-        return password
+    username: Mapped[str] = mapped_column(
+        String(50),
+        unique=True,
+        nullable=False,
+    )
+    password: Mapped[bytes] = mapped_column(
+        nullable=False,
+    )
+    role: Mapped[UserRole] = mapped_column(
+        Enum(UserRole),
+        default=UserRole.USER,
+    )
