8096224c918c4e493e52b98c4308fd7a962ded3c
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-01-29 13:22:44 +0300

    update crud

--- fastapi-kgf/crud/user.py
+++ fastapi-kgf/crud/user.py
@@ -1,57 +1,33 @@
-from collections.abc import Sequence
-
-from sqlalchemy import Row, delete, select
+from sqlalchemy import delete, select
 from sqlalchemy.orm import Session
 
 from core.models.user import User
-from core.schemas import UserCreate
-
-from .user_exceptions import UserAlreadyExistsError
 
 
 def get_all_users(
     session: Session,
-) -> Sequence[Row[tuple[str, str]]] | None:
-    stmt = select(User.username, User.role).order_by(User.id)
-    result = session.execute(stmt)
-    return result.all()
+) -> list[User]:
+    """Возвращает всех пользователей как объект User"""
+    stmt = select(User).order_by(User.id)
+    return list(session.scalars(stmt).all())
 
 
 def get_user_by_username(
     session: Session,
     username: str,
-) -> tuple[User] | None:
-    stmt = select(User).where(User.username == username)
-    result = session.execute(stmt)
-    return result.first()
-
-
-def check_user_exist(
-    session: Session,
-    username: str,
 ) -> User | None:
+    """Возвращает пользователя по username"""
     stmt = select(User).where(User.username == username)
-    return session.scalar(stmt)
+    return session.scalars(stmt).first()
 
 
 def create_user(
     session: Session,
-    user_in: UserCreate,
-) -> User | None:
-    user = User(**user_in.model_dump())
-
-    if (
-        user_in.role == "admin"
-        and check_user_exist(session, user_in.username) is not None
-    ):
-        return None
-
-    if check_user_exist(session, user.username) is not None:
-        raise UserAlreadyExistsError(user_in.username)
-
+    user: User,
+) -> User:
+    """Создает пользователя в БД"""
     session.add(user)
     session.commit()
-
     return user
 
 
@@ -59,6 +35,7 @@ def delete_user(
     session: Session,
     username: str,
 ) -> None:
+    """Удаляет пользователя"""
     stmt = delete(User).where(User.username == username)
     session.execute(stmt)
     session.commit()
