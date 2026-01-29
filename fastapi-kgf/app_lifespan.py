e90e17613abc2212b797b715483fb86c0373776a
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-01-29 13:10:35 +0300

    new logic for create admin

--- fastapi-kgf/app_lifespan.py
+++ fastapi-kgf/app_lifespan.py
@@ -4,29 +4,35 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from core.config import settings
-from core.models import Base, db_helper
-from core.schemas.user import UserCreate
-from crud.user import create_user
-
-
-def create_initial_admin() -> None:
-    session_gen = db_helper.session_getter()
-    session = next(session_gen)
-
-    user_admin = UserCreate(
-        username=settings.admin.username,
-        password=settings.admin.password,
-        role=settings.admin.role,
-    )
-    create_user(session=session, user_in=user_admin)
+from core.models import Base, User, db_helper
+from crud import user as user_crud
+from utils.password_service import hash_password
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncIterator[None]:
-
     Base.metadata.create_all(bind=db_helper.engine)
 
-    create_initial_admin()
+    with db_helper.session_factory() as session:
+        admin = user_crud.get_user_by_username(
+            session=session,
+            username=settings.admin.username,
+        )
+
+        if not admin:
+            hashed_password = hash_password(settings.admin.password)
+
+            admin_user = User(
+                username=settings.admin.username,
+                password=hashed_password,
+                role=settings.admin.role,
+            )
+
+            session.add(admin_user)
+            session.commit()
+
+    # Создаем папки
+    settings.uploads_program_dir.mkdir(exist_ok=True, parents=True)
 
     yield None
 
