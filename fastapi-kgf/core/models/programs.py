424fef5ffabb0a271ad167e74739f95e2eb732f8
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-01-29 13:16:49 +0300

    add settings for field

--- fastapi-kgf/core/models/programs.py
+++ fastapi-kgf/core/models/programs.py
@@ -1,10 +1,13 @@
-from sqlalchemy.orm import Mapped
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
 
 
 class Program(Base):
-    name: Mapped[str]
-    description: Mapped[str]
-    folder_path: Mapped[str]
-    file_size: Mapped[str]
+    __tablename__ = "programs"
+
+    name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=False)
+    folder_path: Mapped[str] = mapped_column(String(500), nullable=False)
+    file_size: Mapped[str] = mapped_column(nullable=False)
