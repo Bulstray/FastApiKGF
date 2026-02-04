7e2008b706ff0b78e4a80c7f6d226d8457a44c81
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-04 13:54:15 +0300

    include tenders router

--- fastapi-kgf/rest/__init__.py
+++ fastapi-kgf/rest/__init__.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from .main_views import router as main_router
 from .programs import router as programs_router
+from .tenders import router as tenders_router
 
 router = APIRouter()
 
 router.include_router(main_router)
 router.include_router(programs_router)
+router.include_router(tenders_router)
