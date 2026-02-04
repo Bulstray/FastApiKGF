1bd051f1e7cbdc284f743f05615ab374642f2b35
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-04 13:53:20 +0300

    refactor endpoints for tenders

---
+++ fastapi-kgf/rest/tenders.py
@@ -0,0 +1,41 @@
+from typing import Annotated
+
+from fastapi import Query, APIRouter
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
+
+from parsers.core import TenderParseCore
+from templating.jinja_template import templates
+
+
+router = APIRouter(
+    prefix="/tenders",
+)
+
+
+@router.get("/", name="tenders:page")
+def tenders_page(
+    request: Request,
+) -> HTMLResponse:
+
+    return templates.TemplateResponse(
+        request=request,
+        name="tenders.html",
+        context={"tenders": []},
+    )
+
+
+@router.get("/search", name="tenders:search")
+def tenders_search(
+    request: Request,
+    search: Annotated[str, Query()],
+) -> HTMLResponse:
+    tender_data = TenderParseCore(key_word=search)
+    context = {
+        "tenders": tender_data.search_all_platforms(),
+    }
+    return templates.TemplateResponse(
+        request=request,
+        name="tenders.html",
+        context=context,
+    )
