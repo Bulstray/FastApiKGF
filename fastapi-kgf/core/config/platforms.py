fd094816cbd51ee5189a23557f2a68ef4d48a39f
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-05 10:04:11 +0300

    rename class to platform

--- fastapi-kgf/core/config/platforms.py
+++ fastapi-kgf/core/config/platforms.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 
 
-class TenderPlatformConfig(BaseModel):
+class PlatformConfig(BaseModel):
     etp_gpb: str = "https://new.etpgpb.ru/procedures.rss"
     tek_torg: str = "https://www.tektorg.ru/procedures"
