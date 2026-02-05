dc22374a2b0eca1ba4d67b30d49fe227511fc776
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-05 09:56:04 +0300

    new file for settings

---
+++ fastapi-kgf/core/config/settings.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from .admin import AdminConfig
+from .api import ApiConfig
+from .database import DatabaseConfig
+from .platforms import TenderPlatformConfig
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=("env.template", ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
+    admin: AdminConfig
+    db: DatabaseConfig = DatabaseConfig()
+    api: ApiConfig = ApiConfig()
+
+    tender_platform: TenderPlatformConfig = TenderPlatformConfig()
+
+    uploads_program_dir: Path = Path("uploads/programs")
+
+
+settings = Settings()  # type: ignore
