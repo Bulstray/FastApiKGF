4eaa35b336413f2fe6fa307d75ed7607af742151
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-06 12:11:56 +0300

    create auth router

--- fastapi-kgf/api/api_v1/__init__.py
+++ fastapi-kgf/api/api_v1/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 
 from core.config import settings
 from dependencies.auth import admin_auth_dependency
+from .auth import router as auth_router
 
 from .programs import router as program_router
 from .users import router as users_router
@@ -16,6 +17,10 @@ router = APIRouter(
 router.include_router(
     program_router,
     prefix=settings.api.v1.programs,
+)
+
+router.include_router(
+    auth_router,
 )
 
 # router.include_router(
