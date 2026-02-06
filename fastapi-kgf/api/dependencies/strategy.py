e0b3d078f92ab1f06541982903328b3df6781276
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-06 10:49:30 +0300

    create get db strategy dependencies

---
+++ fastapi-kgf/api/dependencies/strategy.py
@@ -0,0 +1,23 @@
+from fastapi import Depends
+from fastapi_users.authentication.strategy.db import (
+    AccessTokenDatabase,
+    DatabaseStrategy,
+)
+
+from typing import Annotated
+
+from core.models import AccessToken
+from core.config import settings
+from .access_tokens import get_access_tokens_db
+
+
+def get_database_strategy(
+    access_token_db: Annotated[
+        AccessTokenDatabase[AccessToken],
+        Depends(get_access_tokens_db),
+    ],
+) -> DatabaseStrategy:
+    return DatabaseStrategy(
+        access_token_db,
+        lifetime_seconds=settings.access_token.lifetime_seconds,
+    )
