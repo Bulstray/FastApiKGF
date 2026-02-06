c15e2cee3a7b0202e5669281ef8d5dc72b2c099a
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-06 12:17:24 +0300

    include auth router

--- fastapi-kgf/api/api_v1/auth.py
+++ fastapi-kgf/api/api_v1/auth.py
@@ -1,7 +1,15 @@
 from fastapi import APIRouter
 from core.config import settings
+from .fastapi_users_router import fastapi_users_router
+from api.dependencies.authentication.backend import authentication_backend
 
 router = APIRouter(
     prefix=settings.api.v1.auth,
     tags=["Auth"],
+)
+
+router.include_router(
+    router=fastapi_users_router.get_auth_router(
+        authentication_backend,
+    ),
 )
