4be042180db2fe5cbcfecf205845ee1e7651cb2d
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-06 13:04:24 +0300

    include UserRead, UserCreate and UserUpdate in schemas package

--- fastapi-kgf/core/schemas/__init__.py
+++ fastapi-kgf/core/schemas/__init__.py
@@ -2,7 +2,11 @@ __all__ = (
     "ProgramCreate",
     "ProgramRead",
     "Tender",
+    "UserRead",
+    "UserUpdate",
+    "UserCreate",
 )
 
 from .programs import ProgramCreate, ProgramRead
 from .tenders import Tender
+from .user import UserRead, UserUpdate, UserCreate
