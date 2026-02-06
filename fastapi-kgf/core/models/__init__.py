93081a73e4c5662ebd8bd2307cad62a319541fc9
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-06 10:20:51 +0300

    include access_token model in models package

--- fastapi-kgf/core/models/__init__.py
+++ fastapi-kgf/core/models/__init__.py
@@ -4,10 +4,12 @@ __all__ = (
     "Tender",
     "User",
     "db_helper",
+    "AccessToken",
 )
 
 from .base import Base
 from .db_helper import db_helper
 from .programs import Program
-from .tenders import Tender
+from core.schemas.tenders import Tender
 from .user import User
+from .access_token import AccessToken
