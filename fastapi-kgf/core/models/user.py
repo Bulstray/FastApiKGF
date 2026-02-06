d2998792b174a297d6f3130f22ab22138a0a1f96
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-06 09:46:38 +0300

    create User model

--- fastapi-kgf/core/models/user.py
+++ fastapi-kgf/core/models/user.py
@@ -1,23 +1,12 @@
 from sqlalchemy import Enum, String
 from sqlalchemy.orm import Mapped, mapped_column
 
+from fastapi_users.db import SQLAlchemyBaseUserTable
+
 from core.enums import UserRole
 
 from .base import Base
 
 
-class User(Base):
-    __tablename__ = "user"
-
-    username: Mapped[str] = mapped_column(
-        String(50),
-        unique=True,
-        nullable=False,
-    )
-    password: Mapped[bytes] = mapped_column(
-        nullable=False,
-    )
-    role: Mapped[UserRole] = mapped_column(
-        Enum(UserRole),
-        default=UserRole.USER,
-    )
+class User(Base, SQLAlchemyBaseUserTable[int], Base):
+    pass
