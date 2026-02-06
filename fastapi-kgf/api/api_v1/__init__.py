1a822cc912cf3d7e50b9b2b8662d7963657d1910
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-06 13:00:05 +0300

    remove unuse imports

--- fastapi-kgf/api/api_v1/__init__.py
+++ fastapi-kgf/api/api_v1/__init__.py
@@ -1,17 +1,13 @@
 from fastapi import APIRouter, Depends
 
 from core.config import settings
-from dependencies.auth import admin_auth_dependency
 from .auth import router as auth_router
 
 from .programs import router as program_router
-from .users import router as users_router
+
 
 router = APIRouter(
     prefix=settings.api.v1.prefix,
-    dependencies=[
-        # Depends(admin_auth_dependency),
-    ],
 )
 
 router.include_router(
@@ -22,8 +18,3 @@ router.include_router(
 router.include_router(
     auth_router,
 )
-
-# router.include_router(
-#     users_router,
-#     prefix=settings.api.v1.users,
-# )
