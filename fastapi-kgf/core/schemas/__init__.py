ce6514e7e1b5d4d2b92a7a33385a78b8eed7e511
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-06 12:58:48 +0300

    remove unuse imports

--- fastapi-kgf/core/schemas/__init__.py
+++ fastapi-kgf/core/schemas/__init__.py
@@ -1,12 +1,8 @@
 __all__ = (
     "ProgramCreate",
     "ProgramRead",
-    "User",
-    "UserCreate",
-    "UserRead",
     "Tender",
 )
 
 from .programs import ProgramCreate, ProgramRead
-from .user import User, UserCreate, UserRead
 from .tenders import Tender
