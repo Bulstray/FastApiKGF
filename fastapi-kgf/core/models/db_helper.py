659f7d0480db275897e10b125faa584b160999da
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-09 15:35:11 +0300

    isort

--- fastapi-kgf/core/models/db_helper.py
+++ fastapi-kgf/core/models/db_helper.py
@@ -1,9 +1,9 @@
-from typing import AsyncGenerator
+from collections.abc import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import (
     AsyncSession,
-    create_async_engine,
     async_sessionmaker,
+    create_async_engine,
 )
 
 from core.config import settings
