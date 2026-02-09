6f5b28045574c2bd3251bb5856611a44b7153668
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-09 15:35:00 +0300

    remove tender on models package

--- fastapi-kgf/core/models/__init__.py
+++ fastapi-kgf/core/models/__init__.py
@@ -1,11 +1,10 @@
 __all__ = (
     "Base",
     "Program",
-    "Tender",
     "db_helper",
 )
 
 from .base import Base
 from .db_helper import db_helper
 from .programs import Program
-from core.schemas.tenders import Tender
+from .user import User
