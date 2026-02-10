240a703ce9ea0fda427350bb66df9bc837468c91
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-10 15:38:18 +0300

    update annotation

--- fastapi-kgf/utils/password_service.py
+++ fastapi-kgf/utils/password_service.py
@@ -3,11 +3,11 @@ import bcrypt
 CANNOT_BE_EMPTY = "Password cannot be empty"
 
 
-def hash_password(password: str) -> bytes:
+def hash_password(password: str) -> str:
     """Password Hashing"""
     if not password:
         raise ValueError(CANNOT_BE_EMPTY)
 
     password_bytes = password.encode("utf-8")
     salt = bcrypt.gensalt()
-    return bcrypt.hashpw(password_bytes, salt)
+    return bcrypt.hashpw(password_bytes, salt).decode("utf-8")
