ea6a9eb5c0464f3a20ec62d379cbc4f5b9e6ae5e
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-10 15:32:55 +0300

    include admin auth

--- fastapi-kgf/api/api_v1/__init__.py
+++ fastapi-kgf/api/api_v1/__init__.py
@@ -1,12 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from core.config import settings
+from dependencies.auth_admin import validate_basic_auth
 
 from .programs import router as program_router
 from .user import router as user_router
 
 router = APIRouter(
     prefix=settings.api.v1.prefix,
+    dependencies=[
+        Depends(validate_basic_auth),
+    ],
 )
 
 router.include_router(
