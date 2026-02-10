f0699d30952884e0b2b3e3d7fa255150961c6b9f
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-10 15:38:38 +0300

    create admin then start app

--- fastapi-kgf/app_lifespan.py
+++ fastapi-kgf/app_lifespan.py
@@ -4,13 +4,31 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from core.config import settings
-from core.models import Base, db_helper
+from core.models import Base, User, db_helper
+from crud import user
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncIterator[None]:
     async with db_helper.engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+    async with db_helper.session_factory() as session:
+
+        check_user = await user.get_user_by_username(
+            session=session, username=settings.superuser.username,
+        )
+
+        if check_user is None:
+            admin = User(
+                username=settings.superuser.username,
+                hashed_password=settings.superuser.hashed_password,
+                role=settings.superuser.role,
+            )
+            await user.create_user(
+                session=session,
+                user=admin,
+            )
 
     # Создаем папки
     await settings.uploads_program_dir.mkdir(exist_ok=True, parents=True)
