e96c79346f9bbe519a1e80623140d12fda7037ed
Ramil Khakimov <bulstray@yandex.ru>
2026-02-10 23:53:15 +0300

    include func for is_auth for template

--- fastapi-kgf/templating/jinja_template.py
+++ fastapi-kgf/templating/jinja_template.py
@@ -2,4 +2,8 @@ from fastapi.templating import Jinja2Templates
 
 from core.config import BASE_DIR
 
+from misc.auth import is_authenticated
+
 templates = Jinja2Templates(directory=BASE_DIR / "templates")
+
+templates.env.globals[is_authenticated.__name__] = is_authenticated
