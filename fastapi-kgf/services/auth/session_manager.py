60aad072f3f745b110792f4e1b78c07c54dfea14
Ramil Khakimov <bulstray@yandex.ru>
2026-02-12 19:29:44 +0300

    update service create session

--- fastapi-kgf/services/auth/session_manager.py
+++ fastapi-kgf/services/auth/session_manager.py
@@ -1,26 +1,27 @@
 import uuid
-from time import time
 
-from fastapi import Request
-
-from core.config.settings import ACTIVE_SESSION, SESSION_COOKIE_NAME
 from core.models import User
 from core.schemas.user import UserRead
+
+from storage.session.session import SessionStorage
 
 
 class SessionManager:
     @staticmethod
     def create_session(user: User):
 
-        user_read = UserRead.model_validate(user)
+        session_id = uuid.uuid4()
 
-        session_id = uuid.uuid4().hex
-
-        ACTIVE_SESSION[session_id] = {**user_read.model_dump()}
+        SessionStorage.save_session(
+            session_id=f"{session_id}",
+            user=UserRead.model_validate(user),
+        )
 
         return session_id
 
     @staticmethod
-    def delete_session(session_id: str, request: Request):
-        ACTIVE_SESSION.pop(session_id, None)
-        request.cookies.pop(SESSION_COOKIE_NAME)
+    def delete_session(session_id: str):
+
+        SessionStorage.delete_by_session_id(
+            session_id=session_id,
+        )
