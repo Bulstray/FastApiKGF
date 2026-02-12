39d0e118034795da6927b8107e69f91d043be7e4
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-12 14:34:40 +0300

    include func for jinja

--- fastapi-kgf/templating/jinja_template.py
+++ fastapi-kgf/templating/jinja_template.py
@@ -1,9 +1,8 @@
 from fastapi.templating import Jinja2Templates
 
 from core.config import BASE_DIR
-
-from misc.auth import is_authenticated
+from dependencies.session_auth import get_authenticated_user
 
 templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
-templates.env.globals[is_authenticated.__name__] = is_authenticated
+templates.env.globals[get_authenticated_user.__name__] = get_authenticated_user
