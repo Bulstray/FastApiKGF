81589df41cdb8bf89aad99724183ad828f63dfa9
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-12 15:42:45 +0300

    rename func

--- fastapi-kgf/dependencies/auth_user.py
+++ fastapi-kgf/dependencies/auth_user.py
@@ -2,7 +2,7 @@ import bcrypt
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import User
-from crud import user
+from storage import crud_user
 
 
 async def validate_basic_auth_user(
@@ -10,7 +10,7 @@ async def validate_basic_auth_user(
     password: str,
     session: AsyncSession,
 ) -> User | bool:
-    user_in_db = await user.get_user_by_username(
+    user_in_db = await crud_user.get_user_by_username(
         session=session,
         username=username,
     )
