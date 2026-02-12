9224e7ae20979accac444485b9b986a1e017d164
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-12 14:29:33 +0300

    add new field

--- fastapi-kgf/core/config/superuser.py
+++ fastapi-kgf/core/config/superuser.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from pydantic import AfterValidator, BaseModel
+from pydantic import AfterValidator, BaseModel, EmailStr
 
 from core.types import UserRole
 from utils import hash_password
@@ -11,4 +11,7 @@ PasswordBytes = Annotated[str, AfterValidator(hash_password)]
 class SuperUserConfig(BaseModel):
     username: str
     hashed_password: PasswordBytes
+    name: str
+    surname: str
+    email: EmailStr
     role: UserRole = UserRole.admin
