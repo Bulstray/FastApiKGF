c835e3f0c73cae6fa25d23fc19ccb02556523fe3
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-12 14:31:41 +0300

    remove package

--- fastapi-kgf/misc/auth.py
+++
@@ -1,9 +0,0 @@
-from fastapi import Cookie, Request
-
-from core.config.settings import COOKIES, COOKIES_SESSION_ID_KEY
-
-
-def is_authenticated(
-    request: Request,
-) -> bool:
-    return request.cookies.get(COOKIES_SESSION_ID_KEY) in COOKIES
