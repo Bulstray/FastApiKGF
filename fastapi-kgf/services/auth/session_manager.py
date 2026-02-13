2682a588cb19c1ffd3074a9f768b58c0d310a306
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-13 12:52:13 +0300

    update annotation

--- fastapi-kgf/services/auth/session_manager.py
+++ fastapi-kgf/services/auth/session_manager.py
@@ -2,15 +2,14 @@ import uuid
 
 from core.models import User
 from core.schemas.user import UserRead
-
-from storage.session.session import SessionStorage
+from storage.redis.session import SessionStorage
 
 
 class SessionManager:
     @staticmethod
-    def create_session(user: User):
+    def create_session(user: User) -> str:
 
-        session_id = uuid.uuid4()
+        session_id = uuid.uuid4().hex
 
         SessionStorage.save_session(
             session_id=f"{session_id}",
@@ -20,7 +19,7 @@ class SessionManager:
         return session_id
 
     @staticmethod
-    def delete_session(session_id: str):
+    def delete_session(session_id: str) -> None:
 
         SessionStorage.delete_by_session_id(
             session_id=session_id,
