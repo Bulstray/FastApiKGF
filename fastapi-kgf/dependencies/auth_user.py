7358662119c6e00df2fb7de95230c2645dbdc84f
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-13 12:43:40 +0300

    update annotation

--- fastapi-kgf/dependencies/auth_user.py
+++ fastapi-kgf/dependencies/auth_user.py
@@ -2,26 +2,26 @@ import bcrypt
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import User
-from storage import crud_user
+from storage.db import crud_user
 
 
 async def validate_basic_auth_user(
     username: str,
     password: str,
     session: AsyncSession,
-) -> User | bool:
+) -> User | None:
     user_in_db = await crud_user.get_user_by_username(
         session=session,
         username=username,
     )
 
     if user_in_db is None:
-        return False
+        return None
 
     if not bcrypt.checkpw(
         password=password.encode("utf-8"),
         hashed_password=user_in_db.hashed_password.encode("utf-8"),
     ):
-        return False
+        return None
 
     return user_in_db
