66c9ce8fd90f30317e5343cedc8a08670e0e4073
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-16 15:08:31 +0300

    add task for models package

--- fastapi-kgf/core/models/__init__.py
+++ fastapi-kgf/core/models/__init__.py
@@ -3,9 +3,11 @@ __all__ = (
     "Program",
     "User",
     "db_helper",
+    "Task",
 )
 
 from .base import Base
 from .db_helper import db_helper
 from .programs import Program
 from .user import User
+from .task import Task
