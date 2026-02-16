db9aa0fa796bd3c34b8f0fc08f920e15ef861ed5
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-02-16 15:10:52 +0300

    include router

--- fastapi-kgf/rest/__init__.py
+++ fastapi-kgf/rest/__init__.py
@@ -4,11 +4,12 @@ from .auth import router as auth_router
 from .home_router import router as main_router
 from .programs import router as programs_router
 from .tenders import router as tenders_router
+from .tasks import router as tasks_router
 
 router = APIRouter(include_in_schema=False)
 
 router.include_router(main_router)
 router.include_router(programs_router)
 router.include_router(tenders_router)
-
 router.include_router(auth_router)
+router.include_router(tasks_router)
