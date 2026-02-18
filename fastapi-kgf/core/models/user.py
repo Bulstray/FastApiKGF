a2458987a878bef986cb1f3c981394e00e5effa8
Ramil Khakimov <bulstray@yandex.ru>
2026-02-18 19:02:53 +0300

    update model

--- fastapi-kgf/core/models/user.py
+++ fastapi-kgf/core/models/user.py
@@ -3,12 +3,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from core.types import UserRole
 
-from typing import TYPE_CHECKING
-
 from .base import Base
-
-if TYPE_CHECKING:
-    from .tasks import Tasks
 
 
 class User(Base):
@@ -23,5 +18,3 @@ class User(Base):
     name: Mapped[str] = mapped_column(nullable=False)
     surname: Mapped[str] = mapped_column(nullable=False)
     email: Mapped[str] = mapped_column(nullable=False)
-
-    tasks_user: Mapped[list["Tasks"]] = relationship(back_populates="executor")
