8b54639636fa45457f638246337186b8d114a858
Ramil Khakimov <bulstray@yandex.ru>
2026-02-26 19:21:53 +0300

    add auth check

--- fastapi-kgf/rest/programs/programs_page.py
+++ fastapi-kgf/rest/programs/programs_page.py
@@ -4,9 +4,11 @@ from fastapi import APIRouter, Depends, Request
 from fastapi.responses import HTMLResponse
 
 from core.schemas import ProgramRead
+from core.schemas.user import UserRead
 from dependencies.providers import get_program_service
 from services.programs import ProgramService
 from templating.jinja_template import templates
+from dependencies.session_auth import require_auth
 
 router = APIRouter()
 
@@ -17,6 +19,10 @@ async def programs_page(
     program_service: Annotated[
         ProgramService,
         Depends(get_program_service),
+    ],
+    is_authenticated: Annotated[
+        UserRead,
+        Depends(require_auth),
     ],
 ) -> HTMLResponse:
     """Render programs listing page."""
@@ -33,5 +39,8 @@ async def programs_page(
     return templates.TemplateResponse(
         request=request,
         name="programs.html",
-        context={"programs": programs_schemas},
+        context={
+            "programs": programs_schemas,
+            "is_authenticated": is_authenticated,
+        },
     )
