8049f9a03b95111cfc28d1a55a868a08904742bc
Ramil Khakimov <bulstray@yandex.ru>
2026-02-26 19:20:01 +0300

    remove general depends

--- fastapi-kgf/rest/programs/__init__.py
+++ fastapi-kgf/rest/programs/__init__.py
@@ -6,12 +6,7 @@ from .programs_page import router as programs_page_router
 from dependencies.session_auth import require_auth
 
 
-router = APIRouter(
-    prefix="/programs",
-    dependencies=[
-        Depends(require_auth),
-    ],
-)
+router = APIRouter(prefix="/programs")
 
 router.include_router(programs_page_router)
 router.include_router(download_router)
