87afa25373218142b1262574d542a98634d29f56
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-03-18 15:40:09 +0300

    add AsyncAttrs base class

--- fastapi-kgf/core/models/base.py
+++ fastapi-kgf/core/models/base.py
@@ -1,3 +1,4 @@
+from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import (
     DeclarativeBase,
     Mapped,
@@ -5,7 +6,7 @@ from sqlalchemy.orm import (
 )
 
 
-class Base(DeclarativeBase):
+class Base(AsyncAttrs, DeclarativeBase):
     __abstract__ = True
 
     id: Mapped[int] = mapped_column(
