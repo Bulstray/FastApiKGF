d81aad209b9a5f7d8514c4f3a8f7fbfbfe0fef73
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-03-18 15:44:42 +0300

    update endpoints for shemas

--- fastapi-kgf/rest/tasks/chat_task.py
+++ fastapi-kgf/rest/tasks/chat_task.py
@@ -1,46 +1,81 @@
 import json
 from typing import Annotated
 
+from aiopath import AsyncPath
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.websockets import WebSocket, WebSocketDisconnect
 
+from core.models import db_helper
 from dependencies.message import get_message_service
+from dependencies.providers import get_user_service
 from services.messages.connection_service import connectionmanager
 from services.messages.message_service import MessageManager
-
+from services.notification.connection_manager import manager
+from services.users.service import UserService
 from storage.db import crud_message
-from core.models import db_helper
+from utils.file_size import get_file_size
 
 router = APIRouter()
 
 
-@router.websocket("/ws/task/{task_id}", name="chat:task")
+@router.websocket("/ws/task/{task_id}/{user_id}", name="chat:task")
 async def websocket_endpoint(
     websocket: WebSocket,
     task_id: int,
+    user_id: int,
     message_service: Annotated[MessageManager, Depends(get_message_service)],
+    user_service: Annotated[UserService, Depends(get_user_service)],
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
 ) -> None:
-    await connectionmanager.connect(websocket, task_id)
+    await connectionmanager.connect(websocket, task_id, user_id)
     try:
         while True:
             # Получаем сообщение от клиента
             data = await websocket.receive_text()
             message_date = json.loads(data)
 
-            await message_service.add_message_in_db(
-                message_data=message_date,
+            user = await user_service.get_user_by_id(
+                int(message_date["author"]),
             )
 
+            file_folder = await message_service.add_message_in_db(message_date)
+
+            user_name = await user.awaitable_attrs.name
+            user_surname = await user.awaitable_attrs.surname
+
+            # Формируем нужные строки
+            full_name = f"{user_name} {user_surname}".strip()
+            initials = f"{user_name[0]}{user_surname[0]}".upper()
+
+            # Обновляем данные для рассылки
+            message_date.update(
+                initials=initials,
+                author=full_name,
+            )
+
+            if file_folder:
+                message_date["file"].pop("content")
+                message_date.update(
+                    file={
+                        "name": message_date["file"]["name"],
+                        "folder_path": file_folder,
+                    },
+                )
+
             # Рассылаем всем в этой задаче (включая отправителя)
-            await connectionmanager.broadcast(data, task_id)
+            await connectionmanager.broadcast(
+                json.dumps(message_date),
+                task_id,
+                session,
+            )
 
     except WebSocketDisconnect:
-        connectionmanager.disconnect(websocket, task_id)
+        connectionmanager.disconnect(websocket, task_id, user_id)
 
 
 @router.get("/chat/{task_id}", name="message:task")
-async def get_message_by_id(
+async def get_messages_by_id(
     task_id: int,
     message_service: Annotated[MessageManager, Depends(get_message_service)],
 ):
@@ -49,14 +84,44 @@ async def get_message_by_id(
         task_id,
     )
 
-    return messages
+    messages_dict = []
+
+    for message in messages:
+        msg = {
+            "id": message.id,
+            "text": message.text,
+            "author": message.user_message.full_name,
+            "created_at": message.created_at,
+            "initials": message.user_message.initials,
+        }
+
+        if message.file:
+
+            size = await get_file_size(AsyncPath(message.file.folder_path))
+            msg.update(
+                file={
+                    "name": message.file.name,
+                    "folder_path": message.file.folder_path,
+                    "size": size,
+                },
+            )
+
+        messages_dict.append(msg)
+
+    return messages_dict
 
 
-@router.post("/mark_read/{task_id}", name="message:mark_read")
+@router.post("/mark_read/{task_id}/{user_id}", name="message:mark_read")
 async def mark_read(
-    task_id: int, session: Annotated[AsyncSession, Depends(db_helper.session_getter)]
+    task_id: int,
+    user_id: int,
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
 ):
-    await crud_message.update_mark_read_message(session, task_id)
+    await crud_message.update_mark_read_message(
+        session=session,
+        task_id=task_id,
+        user_id=user_id,
+    )
 
 
 @router.get("/unread", name="message:unread")
@@ -67,36 +132,12 @@ async def get_unread(
     return res
 
 
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: list[WebSocket] = []
+@router.websocket("/ws/notifications/{user_id}")
+async def websocket_notifications(websocket: WebSocket, user_id: int):
+    await manager.connect(websocket, user_id)
+    try:
+        while True:
+            await websocket.receive_text()
 
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
-
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-
-    async def broadcast(self, message: dict):
-        """Отправить всем подключенным клиентам"""
-        for connection in self.active_connections:
-            try:
-                await connection.send_json(message)
-            except:
-                pass
-
-
-manager = ConnectionManager()
-
-#
-# @router.websocket("/ws/notifications")
-# async def websocket_notifications(websocket: WebSocket):
-#     await manager.connect(websocket)
-#     try:
-#         while True:
-#             # Ждем сообщения от клиента (если нужно)
-#             await websocket.receive_text()
-#
-#     except WebSocketDisconnect:
-#         manager.disconnect(websocket)
+    except WebSocketDisconnect:
+        manager.disconnect(user_id=user_id)
