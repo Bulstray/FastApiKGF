9d67c899ee12200bac672b0cb384d7b89be7c23d
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-03-18 15:41:27 +0300

    update model

--- fastapi-kgf/core/models/message_read_status.py
+++ fastapi-kgf/core/models/message_read_status.py
@@ -1,19 +1,15 @@
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
 from .base import Base
-
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import ForeignKey, Boolean
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from .message import Message
-    from .user import User
 
 
 class MessageReadStatus(Base):
     __tablename__ = "message_read_status"
 
-    message_id: Mapped[int] = mapped_column(
-        ForeignKey("messages.id", ondelete="CASCADE"),
+    task_id: Mapped[int] = mapped_column(
+        ForeignKey("tasks.id", ondelete="CASCADE"),
         nullable=False,
         index=True,
     )
@@ -24,8 +20,4 @@ class MessageReadStatus(Base):
         index=True,
     )
 
-    is_read: Mapped[bool] = mapped_column(
-        Boolean,
-        default=False,
-        nullable=False,
-    )
+    count: Mapped[int] = mapped_column(default=0)
