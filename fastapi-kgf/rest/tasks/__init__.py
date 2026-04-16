5cccfb390ab0161620a5737e88b15b5fd8350114
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-16 14:28:35 +0300

    update imports

--- fastapi-kgf/rest/tasks/__init__.py
+++ fastapi-kgf/rest/tasks/__init__.py
@@ -1,19 +1,13 @@
 from fastapi import APIRouter
-
 from .chat import router as chat_router
-from .notifications import router as notification_router
+from .create import router as create_router
+from .download import router as download_router
 from .task_users import router as task_users_router
-from .create_task import router as create_task_router
-from .delete_task import router as delete_task_router
-from .download_file import router as download_file_router
-from .tasks_page import router as tasks_page_router
+
 
 router = APIRouter(prefix="/tasks")
 
-router.include_router(create_task_router)
-router.include_router(delete_task_router)
-router.include_router(download_file_router)
-router.include_router(tasks_page_router)
 router.include_router(chat_router)
-router.include_router(notification_router)
+router.include_router(create_router)
+router.include_router(download_router)
 router.include_router(task_users_router)
