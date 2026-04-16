6d569a071f47162d843d70a56aacb72eff1d2639
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-16 09:35:34 +0300

    black

--- fastapi-kgf/rest/project/project_delete.py
+++ fastapi-kgf/rest/project/project_delete.py
@@ -17,9 +17,15 @@ router = APIRouter()
 @router.post("/{project_id}/delete")
 async def delete_project(
     project_id: int,
-    service: Annotated["ProjectService", Depends(get_project_service)],
+    service: Annotated[
+        "ProjectService",
+        Depends(get_project_service),
+    ],
 ):
     await service.delete_project(project_id)
     await manager.delete_project_id(project_id)
 
-    return RedirectResponse(url="/", status_code=status.HTTP_303_SEE_OTHER)
+    return RedirectResponse(
+        url="/",
+        status_code=status.HTTP_303_SEE_OTHER,
+    )
