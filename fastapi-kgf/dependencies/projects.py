0e89c1d82156697a461f843b3f0bae2ba16772b3
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-16 13:00:43 +0300

    remove func

--- fastapi-kgf/dependencies/projects.py
+++ fastapi-kgf/dependencies/projects.py
@@ -1,6 +1,11 @@
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from services.projects.service import ProjectService
+from services.users.service import UserService
+
+from dependencies.providers import get_user_service
+
+from storage.db.crud_user import get_all_users
 
 from core.models import db_helper
 
@@ -11,3 +16,11 @@ async def get_project_service(
     session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
 ):
     return ProjectService(session)
+
+
+async def get_projects_data(
+    project_id: int,
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    workers: Annotated[UserService, Depends(get_all_users)],
+):
+    print(workers)
