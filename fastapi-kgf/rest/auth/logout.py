8e04070b3efbb4c4a1f15fe4fff75205b1732fcc
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-16 09:33:19 +0300

    review login page

--- fastapi-kgf/rest/auth/logout.py
+++ fastapi-kgf/rest/auth/logout.py
@@ -2,38 +2,27 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends
 from starlette import status
-from starlette.requests import Request
-from starlette.responses import HTMLResponse, RedirectResponse
+from starlette.responses import RedirectResponse
 
-from core.config.settings import SESSION_COOKIE_NAME
-from core.schemas.user import UserRead
-from dependencies.session_auth import require_auth
 from services.auth.session_manager import delete_session
-from storage.redis.session import delete_by_session_id
 
 router = APIRouter(prefix="/logout")
 
 
 @router.get(
-    "/logout",
+    "/",
     name="auth:logout",
     response_model=None,
 )
 async def logout_page(
-    request: Request,
-    user: Annotated[UserRead, Depends(require_auth)],
+    _: Annotated[
+        None,
+        Depends(delete_session),
+    ],
     return_url: str | None = None,
-) -> HTMLResponse | RedirectResponse:
-    session_id = request.cookies.get(SESSION_COOKIE_NAME)
+) -> RedirectResponse:
 
-    if session_id:
-        await delete_session(session_id)
-
-    redirect = RedirectResponse(
+    return RedirectResponse(
         url=return_url or "/",
         status_code=status.HTTP_303_SEE_OTHER,
     )
-
-    redirect.delete_cookie(SESSION_COOKIE_NAME)
-    await delete_by_session_id(session_id)
-    return redirect
