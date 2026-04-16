30d5d908bb6749fd8e7d46f8e8c1bfb9d76668e6
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-16 13:05:04 +0300

    include page project router

--- fastapi-kgf/rest/project/__init__.py
+++ fastapi-kgf/rest/project/__init__.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter
 from .project_create import router as project_create_router
 from .project_delete import router as project_delete_router
+from .project_page import router as project_page_router
 
-router = APIRouter(prefix="/projects")
+
+router = APIRouter(
+    prefix="/projects",
+)
 
 router.include_router(project_create_router)
 router.include_router(project_delete_router)
+router.include_router(project_page_router)
