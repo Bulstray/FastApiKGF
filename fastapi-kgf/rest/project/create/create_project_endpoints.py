e5fb2b43eae0f0b9db4717224a7f878dc68cc27c
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-16 13:10:27 +0300

    refactor endpoints

---
+++ fastapi-kgf/rest/project/create/create_project_endpoints.py
@@ -0,0 +1,26 @@
+from typing import Annotated
+
+from fastapi import Depends, APIRouter
+from starlette import status
+from starlette.requests import Request
+from starlette.responses import RedirectResponse
+
+from dependencies.projects import get_project_service
+
+from services.projects.service import ProjectService
+
+router = APIRouter()
+
+
+@router.post("/", name="project:create")
+async def create_project(
+    request: Request,
+    service: Annotated["ProjectService", Depends(get_project_service)],
+):
+    async with request.form() as form:
+        project = await service.create_project(form)
+
+    return RedirectResponse(
+        url=f"/projects/{project.id}",
+        status_code=status.HTTP_303_SEE_OTHER,
+    )
