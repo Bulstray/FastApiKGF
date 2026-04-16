daab2bac627debca093869b4acfbb97244f9cf52
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-16 14:24:51 +0300

    cookie for websocket

--- fastapi-kgf/dependencies/session_auth.py
+++ fastapi-kgf/dependencies/session_auth.py
@@ -1,19 +1,19 @@
 from typing import Annotated
 
-from fastapi import Depends, HTTPException, status, Request
+from fastapi import Depends, HTTPException, status, Request, Cookie
 from fastapi.responses import HTMLResponse
 from fastapi.security import APIKeyCookie
+from starlette.websockets import WebSocket
 
 from core.config.settings import SESSION_COOKIE_NAME
 from core.schemas.user import UserRead
 from storage.redis import session
-
+from core.schemas.cookie import Cookies
 
 cookie_scheme = APIKeyCookie(name=SESSION_COOKIE_NAME, auto_error=False)
 
 
 async def get_authenticated_user(
-    request: Request,
     session_id: Annotated[
         str | None,
         Depends(cookie_scheme),
@@ -34,15 +34,23 @@ async def get_current_user(
     return await session.get_by_session_id(session_id)
 
 
-async def get_cookie_for_websocket(
-    websocket: WebSocket, session_id: Annotated[str | None, Cookie()]
+async def get_cookie_websocket(
+    websocket: WebSocket, session_id: Annotated[Cookies, Cookie()]
 ):
-    print(session_id)
+    if session_id and (
+        answer := await session.get_by_session_id(session_id.web_app_session_id)
+    ):
+        return answer
+
+    raise HTTPException(
+        status_code=status.HTTP_303_SEE_OTHER,
+        headers={"Location": "/login"},
+    )
 
 
 async def require_auth(
-    user: Annotated[UserRead | None, Depends(get_authenticated_user)],
-) -> UserRead | HTMLResponse:
+    user: Annotated[None | UserRead, Depends(get_authenticated_user)],
+) -> UserRead:
     if not user:
         raise HTTPException(
             status_code=status.HTTP_303_SEE_OTHER,
