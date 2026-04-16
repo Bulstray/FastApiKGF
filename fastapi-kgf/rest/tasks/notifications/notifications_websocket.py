cc7627a05eac0ca89fcaca7068efaf2936421453
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-16 14:27:25 +0300

    black

--- fastapi-kgf/rest/tasks/notifications/notifications_websocket.py
+++ fastapi-kgf/rest/tasks/notifications/notifications_websocket.py
@@ -1,13 +1,18 @@
 from starlette.websockets import WebSocket, WebSocketDisconnect
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Cookie
+from core.schemas.cookie import Cookies
 from services.notification.connection_manager import manager
+from typing import Annotated
 
 router = APIRouter()
 
 
 @router.websocket("/ws/notifications/{user_id}")
-async def websocket_notifications(websocket: WebSocket, user_id: int) -> None:
+async def websocket_notifications(
+    websocket: WebSocket,
+    user_id: int,
+) -> None:
     await manager.connect(websocket, user_id)
     try:
         while True:
