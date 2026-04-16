328d87f9a68cf323f316c073a0e932570b4ba933
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-16 10:34:53 +0300

    refactor endpoint download

---
+++ fastapi-kgf/rest/tasks/download/download_endpoints.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+from starlette.responses import FileResponse
+
+
+router = APIRouter(prefix="/download")
+
+
+@router.get("/{name}/{file_path:path}", name="tasks:download")
+async def download_file(
+    name: str,
+    file_path: str,
+) -> FileResponse:
+    return FileResponse(
+        file_path,
+        filename=name,
+    )
