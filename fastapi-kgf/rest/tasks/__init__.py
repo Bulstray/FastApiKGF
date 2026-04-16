a0bc06c9471f43a603ad1029ad3810161e470a49
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-16 10:09:38 +0300

    include routers

--- fastapi-kgf/rest/tasks/__init__.py
+++ fastapi-kgf/rest/tasks/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
-from .chat_task import router as chat_task_router
+from .chat import router as chat_router
+from .notifications import router as notification_router
+from .task_users import router as task_users_router
 from .create_task import router as create_task_router
 from .delete_task import router as delete_task_router
 from .download_file import router as download_file_router
@@ -12,5 +14,6 @@ router.include_router(create_task_router)
 router.include_router(delete_task_router)
 router.include_router(download_file_router)
 router.include_router(tasks_page_router)
-
-router.include_router(chat_task_router)
+router.include_router(chat_router)
+router.include_router(notification_router)
+router.include_router(task_users_router)
