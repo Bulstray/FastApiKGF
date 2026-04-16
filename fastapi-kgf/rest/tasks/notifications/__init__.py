176a1ddb4257b7bf800f41485a2fda3912f30dd1
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-16 14:27:06 +0300

    update imports

--- fastapi-kgf/rest/tasks/notifications/__init__.py
+++ fastapi-kgf/rest/tasks/notifications/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from .notifications_endpoint import router as notification_router
+from .notifications_websocket import router as notification_router
 
 router = APIRouter()
 
