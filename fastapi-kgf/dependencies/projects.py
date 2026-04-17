e7c7c4b7c4ed04b988480f7046b20ef12960fe82
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-17 13:44:58 +0300

    update imports

--- fastapi-kgf/dependencies/projects.py
+++ fastapi-kgf/dependencies/projects.py
@@ -1,11 +1,6 @@
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from services.projects.service import ProjectService
-from services.users.service import UserService
-
-from dependencies.providers import get_user_service
-
-from storage.db.crud_user import get_all_users
 
 from core.models import db_helper
 
