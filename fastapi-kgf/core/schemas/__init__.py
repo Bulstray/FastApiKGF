db0ad0147d5c8ea6f6825bd23f0954d4d7fcb627
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-22 11:22:14 +0300

    add schemas for package

--- fastapi-kgf/core/schemas/__init__.py
+++ fastapi-kgf/core/schemas/__init__.py
@@ -2,7 +2,25 @@ __all__ = (
     "ProgramCreate",
     "ProgramRead",
     "Tender",
+    "Cookies",
+    "Message",
+    "MessageReadStatus",
+    "ProjectRead",
+    "ProjectCreate",
+    "TaskRead",
+    "TaskCreate",
+    "TaskUsersCreate",
+    "UserRead",
+    "UserCreate",
+    ""
 )
 
 from .programs import ProgramCreate, ProgramRead
 from .tenders import Tender
+from .cookie import Cookies
+from .message import Message
+from .message_read_status import MessageReadStatus
+from .projects import ProjectRead, ProjectCreate
+from .tasks import TaskRead, TaskCreate
+from .tasks_users import TaskUsersCreate
+from .user import UserRead, UserCreate
