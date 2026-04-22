c59edfebddf1410b6c3f1b5f2cd4f2863c984dd4
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-22 11:23:27 +0300

    add descriptions for schemas

--- fastapi-kgf/core/schemas/message.py
+++ fastapi-kgf/core/schemas/message.py
@@ -1,10 +1,14 @@
 from pydantic import BaseModel, ConfigDict
 
 
-class Message(BaseModel):
+class MessageBase(BaseModel):
+    """The basic model for messages"""
     task_id: int
     text: str
     author: int
     created_at: str
 
+
+class Message(MessageBase):
+    """A model for storing messages"""
     model_config = ConfigDict(extra="ignore")
