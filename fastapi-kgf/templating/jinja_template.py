31a9e5a3892f7e29f073ee4dbe6c373a855cbd22
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-22 11:39:21 +0300

    remove get_auth_user

--- fastapi-kgf/templating/jinja_template.py
+++ fastapi-kgf/templating/jinja_template.py
@@ -1,8 +1,5 @@
 from fastapi.templating import Jinja2Templates
 
 from core.config import BASE_DIR
-from dependencies.session_auth import get_authenticated_user
 
 templates = Jinja2Templates(directory=BASE_DIR / "templates")
-
-templates.env.globals[get_authenticated_user.__name__] = get_authenticated_user
