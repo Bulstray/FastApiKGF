4cca9cd78c404ac85ee573b01e4bce52f304b917
Ramil Khakimov <bulstray@yandex.ru>
2026-04-23 16:29:44 +0300

    in github

--- fastapi-kgf/rest/tasks/chat/chat_websocket.py
+++ fastapi-kgf/rest/tasks/chat/chat_websocket.py
@@ -6,9 +6,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.websockets import WebSocket, WebSocketDisconnect
 
 from core.models import db_helper
+from core.schemas import Message, UserRead
 from dependencies.message import get_message_service
 from dependencies.providers import get_user_service
-from services.messages.connection_service import connectionmanager
+from managers.message_manager import message_manager
 from services.messages.message_service import MessageManager
 from services.users.service import UserService
 
@@ -24,30 +25,26 @@ async def websocket_endpoint(
     user_service: Annotated[UserService, Depends(get_user_service)],
     session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
 ) -> None:
-    await connectionmanager.connect(websocket, task_id, user_id)
+    await message_manager.connect(websocket, task_id, user_id)
     try:
         while True:
             # Получаем сообщение от клиента
             data = await websocket.receive_text()
             message_date = json.loads(data)
+            message_schema = Message.model_validate(message_date)
 
-            user = await user_service.get_user_by_id(
-                int(message_date["author"]),
+            user = await user_service.get_by_id(
+                message_schema.author,
             )
+
+            user_schema = UserRead.model_validate(user)
 
             file_folder = await message_service.add_message_in_db(message_date)
 
-            user_name = await user.awaitable_attrs.name
-            user_surname = await user.awaitable_attrs.surname
-
-            # Формируем нужные строки
-            full_name = f"{user_name} {user_surname}".strip()
-            initials = f"{user_name[0]}{user_surname[0]}".upper()
-
             # Обновляем данные для рассылки
             message_date.update(
-                initials=initials,
-                author=full_name,
+                initials=user_schema.initials,
+                author=user_schema.full_name,
             )
 
             if file_folder:
@@ -60,11 +57,11 @@ async def websocket_endpoint(
                 )
 
             # Рассылаем всем в этой задаче (включая отправителя)
-            await connectionmanager.broadcast(
+            await message_manager.broadcast(
                 json.dumps(message_date),
                 task_id,
                 session,
             )
 
     except WebSocketDisconnect:
-        connectionmanager.disconnect(websocket, task_id, user_id)
+        await message_manager.disconnect(websocket, task_id, user_id)
