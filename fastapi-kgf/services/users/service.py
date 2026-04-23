b80b0f4f9ba71348b7199427e93747ee88ddc0f2
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-23 12:06:19 +0300

    update class

--- fastapi-kgf/services/users/service.py
+++ fastapi-kgf/services/users/service.py
@@ -1,12 +1,12 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.models.user import User
-from storage.db.crud_user import get_all_users as crud_get_all_users
+from core.models import User
+from services.base import BaseService
 
 
-class UserService:
+class UserService(BaseService):
+    """Service for users."""
+
     def __init__(self, session: AsyncSession) -> None:
-        self.session = session
-
-    async def get_all_users(self) -> list[User]:
-        return await crud_get_all_users(session=self.session)
+        """Initialize the service with a database connection and user class"""
+        super().__init__(session=session, model=User)
