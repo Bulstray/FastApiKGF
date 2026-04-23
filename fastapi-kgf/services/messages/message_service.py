5790d9acef8223e52b390e3f99e4ff57d265c781
Ramil Khakimov <bulstray@yandex.ru>
2026-04-23 22:17:46 +0300

    update logic

--- fastapi-kgf/services/messages/message_service.py
+++ fastapi-kgf/services/messages/message_service.py
@@ -4,15 +4,16 @@ from core.models import MessageFile, Message
 from services.files import FilesService
 from storage.db import crud_message, crud_task_users
 from core.schemas.message import Message as MessageSchema
+from services.service_base import BaseService
 
 
-class MessageManager:
+class MessageManager(BaseService):
     def __init__(
         self,
         session: AsyncSession,
         file_service: FilesService,
     ) -> None:
-        self.session = session
+        super().__init__(session, Message)
         self.file_service = file_service
 
     async def get_messages_for_task(self, task_id: int) -> list[Message]:
@@ -30,61 +31,61 @@ class MessageManager:
             task_id,
         )
 
-    async def add_message_in_db(
+    async def set_unread_count_message(self, task_id: int, author_id: int) -> None:
+        users_in_task = await crud_task_users.get_task_users(
+            self.session,
+            task_id,
+        )
+
+        for user_id in users_in_task:
+            if author_id == user_id:
+                continue
+            await crud_message.update_mark_read_message(
+                session=self.session,
+                task_id=task_id,
+                user_id=user_id,
+            )
+
+    async def process_message(
         self,
-        message_data: dict[str, str | bytes],
+        message_in: MessageSchema,
     ) -> str | None:
         """
         Создать сообщение с опциональным файлом и обновить счётчики непрочитанных сообщений.
 
         Args:
-            message_data: Данные сообщения, включая опциональные данные файла
+            message_in: Данные сообщения, включая опциональные данные файла
 
         Returns:
             Словарь с ID сообщения и путём к файлу (если файл был загружен)
         """
 
-        message_in = MessageSchema.model_validate(message_data)
+        file_path = None
 
-        message_in_db = await crud_message.create_chats_message(
-            session=self.session,
-            message_in=message_in,
-        )
-
-        users_in_task = await crud_task_users.get_task_users(
-            session=self.session,
-            task_id=message_in.task_id,
-        )
-
-        for user_id in users_in_task:
-            if message_in.author == user_id:
-                continue
-
-            await crud_message.update_count_unread(
-                session=self.session,
-                task_id=message_in.task_id,
-                users_id=user_id,
+        message = Message(
+            **message_in.model_dump(
+                exclude={"file"},
             )
+        )
 
-        if file_data := message_data.get("file"):
-
+        if message_in.file:
             file_path = await self.file_service.save_program_file_bs64(
-                code_file=file_data.get("content"),
-                filename=file_data.get("name"),
+                code_file=message_in.file.content,
+                filename=message_in.file.name,
             )
+            file = MessageFile(name=message_in.file.name, folder_path=f"{file_path}")
 
-            file = MessageFile(
-                name=file_data.get("name"),
-                folder_path=f"{file_path}",
-                message_id=message_in_db,
-            )
+            message.file = file
 
-            file_folder = await crud_message.create_file_data_message(
-                session=self.session,
-                file=file,
-            )
+        await self.create(message)
 
-            return file_folder.folder_path
+        await self.set_unread_count_message(
+            task_id=message_in.task_id,
+            author_id=message_in.author_id,
+        )
+
+        if file_path:
+            return str(file_path)
 
     async def get_unread_message(self, user_id: int) -> dict:
         return await crud_message.get_unread_message(
