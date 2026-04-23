951abce404b8c9686351ec8c0207a563e16e200a
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-23 15:15:58 +0300

    remove method get_all_users

--- fastapi-kgf/storage/db/crud_user.py
+++ fastapi-kgf/storage/db/crud_user.py
@@ -4,13 +4,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from core.models.user import User
 
 
-async def get_all_users(session: AsyncSession) -> list[User]:
-    """Возвращает всех пользователей"""
-    stmt = select(User).order_by(User.id)
-    result = await session.scalars(stmt)
-    return list(result.all())
-
-
 async def get_user_by_username(
     session: AsyncSession,
     username: str,
