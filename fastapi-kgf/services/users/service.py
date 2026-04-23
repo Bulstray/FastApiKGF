df3fc273a0d7b9cba4af7fb5d7fb27ee3ede926d
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-23 15:17:58 +0300

    remove arg

--- fastapi-kgf/services/users/service.py
+++ fastapi-kgf/services/users/service.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import User
-from services.base import BaseService
+from services.service_base import BaseService
 
 
 class UserService(BaseService):
@@ -9,4 +9,4 @@ class UserService(BaseService):
 
     def __init__(self, session: AsyncSession) -> None:
         """Initialize the service with a database connection and user class"""
-        super().__init__(session=session, model=User)
+        super().__init__(session, User)
