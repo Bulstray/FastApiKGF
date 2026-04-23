0bf5202c8e57b6cfc9af52696d5c5f6900173603
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-23 15:16:33 +0300

    add new model

--- fastapi-kgf/core/schemas/message.py
+++ fastapi-kgf/core/schemas/message.py
@@ -1,14 +1,22 @@
 from pydantic import BaseModel, ConfigDict
 
 
+class MessageFile(BaseModel):
+    name: str
+    content: bytes
+
+
 class MessageBase(BaseModel):
     """The basic model for messages"""
+
     task_id: int
     text: str
     author: int
     created_at: str
+    file: MessageFile | None = None
 
 
 class Message(MessageBase):
     """A model for storing messages"""
+
     model_config = ConfigDict(extra="ignore")
