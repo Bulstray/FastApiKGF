c2764decb87f71ef4e292e1ae19ab760d28dfeb2
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-27 14:59:01 +0300

    class user_service

--- fastapi-kgf/dependencies/auth_user.py
+++ fastapi-kgf/dependencies/auth_user.py
@@ -1,27 +1,26 @@
 import bcrypt
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from typing import Annotated
 from fastapi import Depends, Request
 
-from core.models import db_helper
 
 from core.models import User
-from storage.db import crud_user
+from services.users.service import UserService
+
+from .user import get_user_service
 
 from services.auth.session_manager import create_session
 
 
 async def validate_basic_auth_user(
     request: Request,
-    session: Annotated[
-        AsyncSession,
-        Depends(db_helper.session_getter),
+    user_service: Annotated[
+        UserService,
+        Depends(get_user_service),
     ],
 ) -> str | None:
     async with request.form() as form_data:
-        is_user: None | User = await crud_user.get_user_by_username(
-            session=session,
+        is_user: None | User = await user_service.get_user_by_username(
             username=form_data["username"],
         )
 
