310b7a9a406ed278b5dd098b5bf4d8ac34645e2c
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-27 15:40:16 +0300

    add annotation and isort

--- fastapi-kgf/storage/db/crud_user.py
+++ fastapi-kgf/storage/db/crud_user.py
@@ -2,14 +2,15 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models.user import User
+
 from .base_crud import BaseCRUD
 
 
 class UserStorage(BaseCRUD):
-    def __init__(self, session: AsyncSession):
+    def __init__(self, session: AsyncSession) -> None:
         super().__init__(session, User)
 
-    async def get_user_by_username(self, username: str):
+    async def get_user_by_username(self, username: str) -> User | None:
         stmt = select(User).where(username == User.username)
         result = await self.session.scalars(stmt)
         return result.first()
