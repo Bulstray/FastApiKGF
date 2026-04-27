3e3c608e7602f7adb1d86a9afe76727da401bd14
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-27 15:41:55 +0300

    isort

--- fastapi-kgf/core/models/message_file.py
+++ fastapi-kgf/core/models/message_file.py
@@ -1,6 +1,5 @@
-from typing import TYPE_CHECKING
-
 from pathlib import Path
+from typing import TYPE_CHECKING
 
 from sqlalchemy import ForeignKey, String, event
 from sqlalchemy.orm import Mapped, mapped_column, relationship
