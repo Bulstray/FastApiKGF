ac0ac655304de312ca99dcf0df2b8b2fd73063cd
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-27 15:02:16 +0300

    parent UserStorage

--- fastapi-kgf/services/users/service.py
+++ fastapi-kgf/services/users/service.py
@@ -1,12 +1,11 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.models import User
-from storage.db.base_crud import BaseCRUD
+from storage.db.crud_user import UserStorage
 
 
-class UserService(BaseCRUD):
+class UserService(UserStorage):
     """Service for users."""
 
     def __init__(self, session: AsyncSession) -> None:
         """Initialize the service with a database connection and user class"""
-        super().__init__(session, User)
+        super().__init__(session)
