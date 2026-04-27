c3480d3bf371fecefaa28413ea0c034ab03fcbc8
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-27 15:00:31 +0300

    class UserStorage

--- fastapi-kgf/storage/db/crud_user.py
+++ fastapi-kgf/storage/db/crud_user.py
@@ -2,25 +2,14 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models.user import User
+from .base_crud import BaseCRUD
 
 
-async def get_user_by_username(
-    session: AsyncSession,
-    username: str,
-) -> User | None:
-    """Возвращает пользователя по имени"""
-    stmt = select(User).where(username == User.username)
-    result = await session.scalars(stmt)
-    return result.first()
+class UserStorage(BaseCRUD):
+    def __init__(self, session: AsyncSession):
+        super().__init__(session, User)
 
-
-async def create_user(session: AsyncSession, user: User) -> User:
-    """Создание пользователя"""
-    session.add(user)
-    await session.commit()
-    await session.refresh(user)
-    return user
-
-
-async def get_user_by_id(session: AsyncSession, user_id: int) -> User | None:
-    return await session.get(User, user_id)
+    async def get_user_by_username(self, username: str):
+        stmt = select(User).where(username == User.username)
+        result = await self.session.scalars(stmt)
+        return result.first()
