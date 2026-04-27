9f86ce9bd1b9e06a0cfa059f272098ee559f135e
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-27 15:37:26 +0300

    isort

--- fastapi-kgf/core/schemas/__init__.py
+++ fastapi-kgf/core/schemas/__init__.py
@@ -1,26 +1,25 @@
 __all__ = (
-    "ProgramCreate",
-    "ProgramRead",
-    "Tender",
     "Cookies",
     "Message",
     "MessageReadStatus",
-    "ProjectRead",
+    "ProgramCreate",
+    "ProgramRead",
     "ProjectCreate",
-    "TaskRead",
+    "ProjectRead",
     "TaskCreate",
+    "TaskRead",
     "TaskUsersCreate",
+    "Tender",
     "UserRead",
-    "UserCreate",
-    ""
+    "UserLogin",
 )
 
-from .programs import ProgramCreate, ProgramRead
-from .tenders import Tender
 from .cookie import Cookies
 from .message import Message
 from .message_read_status import MessageReadStatus
-from .projects import ProjectRead, ProjectCreate
-from .tasks import TaskRead, TaskCreate
+from .programs import ProgramCreate, ProgramRead
+from .projects import ProjectCreate, ProjectRead
+from .tasks import TaskCreate, TaskRead
 from .tasks_users import TaskUsersCreate
-from .user import UserRead, UserCreate
+from .tenders import Tender
+from .user import UserRead, UserLogin
