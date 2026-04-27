b9d5868d1aa903170b2e9d4513442be56b9075d8
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-27 15:03:20 +0300

    user_service dependencies

---
+++ fastapi-kgf/dependencies/user.py
@@ -0,0 +1,12 @@
+from fastapi import Depends
+from typing import Annotated
+from sqlalchemy.ext.asyncio import AsyncSession
+from core.models import db_helper
+
+from services.users.service import UserService
+
+
+async def get_user_service(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+):
+    return UserService(session)
