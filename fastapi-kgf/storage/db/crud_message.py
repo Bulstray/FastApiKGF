aa917a13ca73155fd43844f4e794f79013e2d02f
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-27 10:53:51 +0300

    create class for crud message

--- fastapi-kgf/storage/db/crud_message.py
+++ fastapi-kgf/storage/db/crud_message.py
@@ -1,49 +1,39 @@
-from sqlalchemy import and_, func, select, update
+from sqlalchemy import and_, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
-from core.models import Message, MessageFile, MessageReadStatus
-from core.schemas.message import Message as MessageSchema
-from core.schemas.message_read_status import (
-    MessageReadStatus as MessageReadStatusSchema,
-)
+from core.models import Message, MessageReadStatus
+from .base_crud import BaseCRUD
 
 
-async def create_chats_message(
-    session: AsyncSession,
-    message_in: MessageSchema,
-) -> int:
-    message = Message(**message_in.model_dump(exclude={"file"}))
-    session.add(message)
-    await session.flush()
-    message_id = message.id
-    await session.commit()
-    return message_id
+class MessageStorage(BaseCRUD):
+    def __init__(self, session: AsyncSession):
+        super().__init__(session, Message)
 
+    async def get_unread_message(self, user_id: int):
+        stmt = select(
+            MessageReadStatus.task_id,
+            MessageReadStatus.count,
+        ).where(MessageReadStatus.user_id == user_id)
 
-async def create_file_data_message(
-    session: AsyncSession,
-    file: MessageFile,
-) -> MessageFile:
-    session.add(file)
-    await session.commit()
-    await session.refresh(file)
-    return file
+        result = await self.session.execute(stmt)
+        return {
+            task_message.task_id: task_message.count for task_message in result.all()
+        }
 
+    async def get_messages_for_task(
+        self,
+        task_id: int,
+    ) -> list[Message]:
+        message = (
+            select(Message)
+            .where(Message.task_id == task_id)
+            .order_by(Message.id)
+            .options(selectinload(Message.user_message))
+        )
 
-async def get_messages_for_task(
-    session: AsyncSession,
-    task_id: int,
-) -> list[Message]:
-    message = (
-        select(Message)
-        .where(Message.task_id == task_id)
-        .order_by(Message.id)
-        .options(selectinload(Message.user_message))
-    )
-
-    result = await session.scalars(message)
-    return list(result.all())
+        result = await self.session.scalars(message)
+        return list(result.all())
 
 
 async def update_mark_read_message(
@@ -66,28 +56,17 @@ async def update_mark_read_message(
 async def update_count_unread(
     session: AsyncSession,
     task_id: int,
-    users_id: int,
+    user_id: int,
 ) -> None:
     stmt = (
         update(MessageReadStatus)
         .where(
             and_(
                 MessageReadStatus.task_id == task_id,
-                MessageReadStatus.user_id == users_id,
+                MessageReadStatus.user_id == user_id,
             ),
         )
         .values(count=MessageReadStatus.count + 1)
     )
     await session.execute(stmt)
     await session.commit()
-
-
-async def get_unread_message(
-    session: AsyncSession,
-    user_id: int,
-) -> dict[int, int]:
-    stmt = select(MessageReadStatus.task_id, MessageReadStatus.count).where(
-        MessageReadStatus.user_id == user_id,
-    )
-    result = await session.execute(stmt)
-    return {task_message.task_id: task_message.count for task_message in result.all()}
