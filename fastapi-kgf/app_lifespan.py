762d1c9041f56ea6f92a1504fe97a25a9fa9b81a
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-27 14:58:43 +0300

    update logic

--- fastapi-kgf/app_lifespan.py
+++ fastapi-kgf/app_lifespan.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 
 from core.config import settings
 from core.models import Base, User, db_helper
-from storage.db import crud_user
+from services.users.service import UserService
 
 
 @asynccontextmanager
@@ -15,24 +15,15 @@ async def lifespan(app: FastAPI) -> AsyncIterator[None]:
 
     async with db_helper.session_factory() as session:
 
-        check_user = await crud_user.get_user_by_username(
-            session=session,
+        user_service = UserService(session)
+
+        check_user = await user_service.get_user_by_username(
             username=settings.superuser.username,
         )
 
         if check_user is None:
-            admin = User(
-                username=settings.superuser.username,
-                hashed_password=settings.superuser.hashed_password,
-                role=settings.superuser.role,
-                name=settings.superuser.name,
-                email=settings.superuser.email,
-                surname=settings.superuser.surname,
-            )
-            await crud_user.create_user(
-                session=session,
-                user=admin,
-            )
+            admin = User(**settings.superuser.model_dump())
+            await user_service.create(admin)
 
     # Создаем папки
     await settings.uploads_program_dir.mkdir(exist_ok=True, parents=True)
