a4a207f0b454393f9db8544abcc86cd5f2e2bd2a
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-27 15:37:58 +0300

    remove spaces

--- fastapi-kgf/rest/tasks/__init__.py
+++ fastapi-kgf/rest/tasks/__init__.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
+
 from .chat import router as chat_router
 from .create import router as create_router
 from .download import router as download_router
 from .task_users import router as task_users_router
-
 
 router = APIRouter(prefix="/tasks")
 
