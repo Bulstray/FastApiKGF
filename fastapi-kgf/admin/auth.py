78c180b70df9b3f50d1ac53221ed048d9fc96314
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-27 15:38:19 +0300

    isort

--- fastapi-kgf/admin/auth.py
+++ fastapi-kgf/admin/auth.py
@@ -1,11 +1,9 @@
-from sqladmin.authentication import AuthenticationBackend
-from starlette.responses import Response
-
-from dependencies.session_auth import get_authenticated_user
-from core.types import UserRole
 from fastapi import Request
+from sqladmin.authentication import AuthenticationBackend
 
 from core.config.settings import SESSION_COOKIE_NAME
+from core.types import UserRole
+from dependencies.session_auth import get_authenticated_user
 
 
 class AdminAuth(AuthenticationBackend):
