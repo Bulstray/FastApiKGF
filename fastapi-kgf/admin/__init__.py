d0108f5467b16e4e0c42d36310f96476bbb59103
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-28 14:52:58 +0300

    register keyword in admin

--- fastapi-kgf/admin/__init__.py
+++ fastapi-kgf/admin/__init__.py
@@ -2,8 +2,10 @@ from sqladmin import Admin
 
 from .user import UserAdmin
 from .project import ProjectAdmin
+from .parser_keyword import ParserKeywordAdmin
 
 
 def register_admin_views(admin: Admin) -> None:
     admin.add_view(UserAdmin)
     admin.add_view(ProjectAdmin)
+    admin.add_view(ParserKeywordAdmin)
