5e8e2b9f91cb60479197aaa918312bcf1fc2fe5e
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-28 12:30:26 +0300

    add admin project

--- fastapi-kgf/admin/__init__.py
+++ fastapi-kgf/admin/__init__.py
@@ -1,7 +1,9 @@
 from sqladmin import Admin
 
 from .user import UserAdmin
+from .project import ProjectAdmin
 
 
 def register_admin_views(admin: Admin) -> None:
     admin.add_view(UserAdmin)
+    admin.add_view(ProjectAdmin)
