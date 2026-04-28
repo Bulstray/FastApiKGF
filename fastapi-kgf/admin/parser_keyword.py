f86e8ea8e8b54902059ce7de98af21f0f8b03a8b
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-28 14:53:26 +0300

    parserKeywordAdmin

---
+++ fastapi-kgf/admin/parser_keyword.py
@@ -0,0 +1,13 @@
+from sqladmin import ModelView
+from core.models import ParsingKeyword
+
+
+class ParserKeywordAdmin(ModelView, model=ParsingKeyword):
+    column_list = [
+        ParsingKeyword.decoding,
+        ParsingKeyword.keyword,
+    ]
+    form_columns = [
+        ParsingKeyword.decoding,
+        ParsingKeyword.keyword,
+    ]
