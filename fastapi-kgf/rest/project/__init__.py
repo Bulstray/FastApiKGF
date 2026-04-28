3a7ae0510006159c4084247a269093803923d4f6
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-28 12:31:02 +0300

    remove enpoint create and delete

--- fastapi-kgf/rest/project/__init__.py
+++ fastapi-kgf/rest/project/__init__.py
@@ -1,13 +1,9 @@
 from fastapi import APIRouter
 
-from .create import router as router_create_project
-from .delete import router as router_delete_project
 from .page import router as project_page_router
 
 router = APIRouter(
     prefix="/projects",
 )
 
-router.include_router(router_create_project)
-router.include_router(router_delete_project)
 router.include_router(project_page_router)
