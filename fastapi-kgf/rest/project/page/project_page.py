7651a9f6e7248c804ffe10c28a963f941c3c647a
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-28 11:44:34 +0300

    refactor args

--- fastapi-kgf/rest/project/page/project_page.py
+++ fastapi-kgf/rest/project/page/project_page.py
@@ -20,18 +20,38 @@ from templating.jinja_template import templates
 router = APIRouter()
 
 
-async def _build_context(
-    project_id: int,
+async def _get_user_and_user_service(
     user: Annotated[UserRead, Depends(get_current_user)],
     user_service: Annotated[UserService, Depends(get_user_service)],
-    tasks_service: Annotated[TasksFilesService, Depends(get_tasks_service)],
-    message_service: Annotated[MessageManager, Depends(get_message_service)],
-    project_service: Annotated[ProjectService, Depends(get_project_service)],
+) -> tuple[UserRead, UserService]:
+    return user, user_service
+
+
+async def _build_context(
+    project_id: int,
+    user_and_user_service: Annotated[
+        tuple[UserRead, UserService],
+        Depends(_get_user_and_user_service),
+    ],
+    tasks_service: Annotated[
+        TasksFilesService,
+        Depends(get_tasks_service),
+    ],
+    message_service: Annotated[
+        MessageManager,
+        Depends(get_message_service),
+    ],
+    project_service: Annotated[
+        ProjectService,
+        Depends(get_project_service),
+    ],
 ) -> dict[str, Any]:
+    user, user_service = user_and_user_service
     count_unread_message = await message_service.get_unread_message(user.id)
     workers_list = await user_service.get_all()
     tasks_for_users = await tasks_service.get_user_tasks_by_project_id(
-        project_id, user.id,
+        project_id,
+        user.id,
     )
     projects = await project_service.get_all()
     project = await project_service.get_by_id(project_id)
@@ -44,7 +64,7 @@ async def _build_context(
         "projects": projects,
         "project_id": project_id,
         "project": project,
-        "today": datetime.date.today(),
+        "today": datetime.datetime.now(tz=datetime.UTC).date(),
     }
 
 
