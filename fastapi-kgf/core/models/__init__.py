3e7ac3cc9e3059ff4923c404e4163ce3168feb97
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-29 10:03:20 +0300

    isort

--- fastapi-kgf/core/models/__init__.py
+++ fastapi-kgf/core/models/__init__.py
@@ -3,13 +3,13 @@ __all__ = (
     "Message",
     "MessageFile",
     "MessageReadStatus",
+    "ParsingKeyword",
     "Program",
     "Project",
     "Task",
     "TaskUsers",
     "User",
     "db_helper",
-    "ParsingKeyword",
 )
 
 from .base import Base
@@ -17,9 +17,9 @@ from .db_helper import db_helper
 from .message import Message
 from .message_file import MessageFile
 from .message_read_status import MessageReadStatus
+from .parsing_keyword import ParsingKeyword
 from .programs import Program
 from .projects import Project
 from .taks_users import TaskUsers
 from .task import Task
 from .user import User
-from .parsing_keyword import ParsingKeyword
