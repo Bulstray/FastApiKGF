573b144d2b07d535343c4ac6b1b48dd360e65214
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-04-29 15:06:35 +0300

    add headers

--- fastapi-kgf/parsers/platforms/base_platform.py
+++ fastapi-kgf/parsers/platforms/base_platform.py
@@ -5,13 +5,17 @@ import requests
 from bs4.element import ResultSet, Tag
 
 from core.schemas import Tender
-from core.types.platform import Platform
 
 
 class BaseTenderPlatform(ABC):
     """Базовый класс для всех парсеров площадок"""
 
     TIMEOUT = 10
+    HEADERS = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    }
 
     def __init__(
         self,
@@ -23,6 +27,7 @@ class BaseTenderPlatform(ABC):
             url=self.base_url,
             params=params,
             timeout=self.TIMEOUT,
+            headers=self.HEADERS,
         )
 
     @staticmethod
@@ -44,15 +49,26 @@ class BaseTenderPlatform(ABC):
     def is_tender_pub_date_taken(card: Tag | Element) -> str:
         """Метод для проверки и преобразовании даты публикации"""
 
-    def search_tenders(self, platform: Platform) -> list[Tender]:
+    @staticmethod
+    @abstractmethod
+    def is_tender_price_taken(card: Tag | Element) -> str:
+        """Метод для получения цены"""
+
+    @staticmethod
+    @abstractmethod
+    def is_tender_organize_taken(card: Tag | Element) -> str:
+        """Метод для определения организатора"""
+
+    def search_tenders(self) -> list[Tender]:
 
         cards = self.get_cards_data()
 
         return [
             Tender(
-                platform=platform,
                 name=self.is_tender_name_taken(card=card),
                 pub_date=self.is_tender_pub_date_taken(card=card),
+                price=self.is_tender_price_taken(card=card),
+                organizer=self.is_tender_organize_taken(card=card),
             )
             for card in cards
         ]
