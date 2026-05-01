944e36a70396355167fad3249687b735c4088815
Ramil Khakimov <bulstray@yandex.ru>
2026-05-01 18:02:50 +0300

    add header

--- fastapi-kgf/parsers/platforms/base_platform.py
+++ fastapi-kgf/parsers/platforms/base_platform.py
@@ -1,10 +1,18 @@
+import time
 from abc import ABC, abstractmethod
 from xml.etree.ElementTree import Element
 
 import requests
+
+from core.config import settings
+
+
 from bs4.element import ResultSet, Tag
 
 from core.schemas import Tender
+
+from selenium import webdriver
+from urllib.parse import urlencode
 
 
 class BaseTenderPlatform(ABC):
@@ -12,23 +20,33 @@ class BaseTenderPlatform(ABC):
 
     TIMEOUT = 10
     HEADERS = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
         'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Connection': 'keep-alive',
     }
 
     def __init__(
         self,
+        base_platform: str,
         base_url: str,
         params: dict[str, str | int],
     ) -> None:
         self.base_url = base_url
-        self.response = requests.get(
-            url=self.base_url,
-            params=params,
-            timeout=self.TIMEOUT,
-            headers=self.HEADERS,
-        )
+        self.base_platform = base_platform
+        if base_url == settings.tender_platform.etp_gpb:
+            driver = webdriver.Chrome()
+            driver.get(f"{base_url}?{urlencode(params)}")
+            time.sleep(5)
+            self.html_source = driver.page_source
+            driver.quit()
+        else:
+            self.html_source = requests.get(
+                url=self.base_url,
+                params=params,
+                timeout=self.TIMEOUT,
+                headers=self.HEADERS,
+            ).text
 
     @staticmethod
     @abstractmethod
@@ -39,9 +57,10 @@ class BaseTenderPlatform(ABC):
     def get_cards_data(self) -> list[Element] | ResultSet[Tag]:
         """Получение блока с тендерами"""
 
-    @staticmethod
     @abstractmethod
-    def is_tender_name_taken(card: Tag | Element) -> str:
+    def is_tender_name_taken(
+        self, card: Tag | Element
+    ) -> tuple[str, str] | None:
         """Метод для проверки имени тендера"""
 
     @staticmethod
@@ -63,12 +82,22 @@ class BaseTenderPlatform(ABC):
 
         cards = self.get_cards_data()
 
-        return [
-            Tender(
-                name=self.is_tender_name_taken(card=card),
-                pub_date=self.is_tender_pub_date_taken(card=card),
-                price=self.is_tender_price_taken(card=card),
-                organizer=self.is_tender_organize_taken(card=card),
+        tenders = []
+        for card in cards:
+            title_and_url = self.is_tender_name_taken(card)
+            if title_and_url is None:
+                continue
+
+            title, url = title_and_url
+
+            tenders.append(
+                Tender(
+                    name=title,
+                    pub_date=self.is_tender_pub_date_taken(card),
+                    price=self.is_tender_price_taken(card),
+                    organizer=self.is_tender_organize_taken(card),
+                    url=fr"{self.base_platform}{url}",
+                )
             )
-            for card in cards
-        ]
+
+        return tenders
