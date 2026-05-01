9553784dfa6e7267bb7cbce910108d1c901ab8b9
Ramil Khakimov <bulstray@yandex.ru>
2026-05-01 18:03:44 +0300

    refactor

--- fastapi-kgf/core/config/platforms.py
+++ fastapi-kgf/core/config/platforms.py
@@ -1,6 +1,26 @@
 from pydantic import BaseModel
 
 
+class BasePlatform(BaseModel):
+    base_tek_torg: str = "https://www.tektorg.ru"
+    base_etp_gpb: str = "https://new.etpgpb.ru"
+
+
+class PlatformPrefixProcedures(BaseModel):
+    etp_gpb: str = "/procedures"
+    tek_torg: str = "/procedures"
+
+
 class PlatformConfig(BaseModel):
-    etp_gpb: str = "https://new.etpgpb.ru/procedures"
-    tek_torg: str = "https://www.tektorg.ru/procedures"
+    base_platform: BasePlatform = BasePlatform()
+    platform_prefix_procedures: PlatformPrefixProcedures = (
+        PlatformPrefixProcedures()
+    )
+
+    @property
+    def tek_torg(self):
+        return f"{self.base_platform.base_tek_torg}{self.platform_prefix_procedures.tek_torg}"
+
+    @property
+    def etp_gpb(self):
+        return f"{self.base_platform.base_etp_gpb}{self.platform_prefix_procedures.etp_gpb}"
