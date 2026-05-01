395705d4e8962742b501f626fdc11162f4d2dbcb
Ramil Khakimov <bulstray@yandex.ru>
2026-05-01 18:03:02 +0300

    add Etpgpb

--- fastapi-kgf/parsers/core.py
+++ fastapi-kgf/parsers/core.py
@@ -1,5 +1,4 @@
 from core.schemas import Tender
-from core.types.platform import Platform
 
 from .platforms import EtpgpbParser, TekTorgPlatform
 
@@ -7,7 +6,8 @@ from .platforms import EtpgpbParser, TekTorgPlatform
 class TenderParseCore:
     def __init__(self, key_word: str) -> None:
         self.parsers = {
-            (TekTorgPlatform(key_word=key_word)),
+            TekTorgPlatform(key_word=key_word),
+            EtpgpbParser(key_word=key_word),
         }
 
     def search_all_platforms(self) -> list[Tender]:
