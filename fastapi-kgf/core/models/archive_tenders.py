dee83306c0c1da91e775e8f7b584b41bcb750d4e
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-04 15:49:56 +0300

    model archive tender

---
+++ fastapi-kgf/core/models/archive_tenders.py
@@ -0,0 +1,16 @@
+from base import Base
+from sqlalchemy.orm import Mapped
+
+from datetime import datetime
+
+
+class ArchiveTender(Base):
+    __tablename__ = "archive_tenders"
+
+    name: Mapped[str]
+    pub_date: Mapped[str]
+    price: Mapped[str]
+    organizer: Mapped[str]
+    url: Mapped[str]
+    end_date: Mapped[datetime]
+    keyword_id: Mapped[int]
