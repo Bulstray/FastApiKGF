c67e198cd5b8460b42ba8b8ffd8842dd4002792f
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-04 15:53:47 +0300

    update tender model

--- fastapi-kgf/core/models/tender.py
+++ fastapi-kgf/core/models/tender.py
@@ -1,11 +1,32 @@
 from .base import Base
 
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Mapped, relationship, mapped_column
+from sqlalchemy import ForeignKey
+
+from typing import TYPE_CHECKING
+from datetime import datetime
+
+if TYPE_CHECKING:
+    from .parsing_keyword import ParsingKeyword
 
 
 class Tender(Base):
-    __tablename__ = 'tenders'
+    __tablename__ = "tenders"
 
-    title: Mapped[str]
-    organizer: Mapped[str]
+    name: Mapped[str]
     pub_date: Mapped[str]
+    price: Mapped[str]
+    organizer: Mapped[str]
+    url: Mapped[str]
+    end_date: Mapped[datetime]
+    keyword_id: Mapped[int] = mapped_column(
+        ForeignKey(
+            "parsing_keyword.id",
+            ondelete="CASCADE",
+        )
+    )
+
+    keyword: Mapped["ParsingKeyword"] = relationship(
+        "ParsingKeyword",
+        back_populates="tenders",
+    )
