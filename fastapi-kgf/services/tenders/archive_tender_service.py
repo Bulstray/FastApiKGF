43c72afcd2a8340bb263990f3ab7f4d205c4cffa
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-04 15:49:40 +0300

    init archive tender service

---
+++ fastapi-kgf/services/tenders/archive_tender_service.py
@@ -0,0 +1,17 @@
+from storage.db.crud_arhive_tenders import ArchiveTendersStorage
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from core.schemas.tenders import TenderCreate as TenderSchema
+from core.models import Tender, ArchiveTender
+
+
+class ArchiveTendersService(ArchiveTendersStorage):
+    def __init__(self, session: AsyncSession):
+        super().__init__(session)
+
+    async def add_tenders_in_db(self, tenders: list[Tender]) -> None:
+        tenders_models = [
+            ArchiveTender(**TenderSchema.model_validate(tender))
+            for tender in tenders
+        ]
+        await self.add_all(tenders_models)
