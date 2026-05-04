f67a40d8bbcf0e0f9017616223c3e7c541b5acad
Ramil Khakimov <bulstray@yandex.ru>
2026-05-04 07:30:34 +0300

    add relationship

--- fastapi-kgf/core/models/parsing_keyword.py
+++ fastapi-kgf/core/models/parsing_keyword.py
@@ -1,10 +1,21 @@
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Mapped, relationship
+from typing import TYPE_CHECKING
 
 from .base import Base
 
+if TYPE_CHECKING:
+    from .tenders import Tender
+
 
 class ParsingKeyword(Base):
-    __tablename__ = "parsing_keyword"
+    __tablename__ = 'parsing_keyword'
 
     decoding: Mapped[str]
     keyword: Mapped[str]
+
+    tenders: Mapped[list["Tender"]] = relationship(
+        "Tender",
+        back_populates="keyword",
+        cascade="all, delete-orphan",
+        lazy="selectin",
+    )
