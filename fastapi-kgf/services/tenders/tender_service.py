8a3ef8aff83345ac93a15e6c2c4dff769e433e30
Ramil Khakimov <bulstray@yandex.ru>
2026-05-04 07:32:26 +0300

    tenders_service

---
+++ fastapi-kgf/services/tenders/tender_service.py
@@ -0,0 +1,14 @@
+from storage.db.crud_tenders import TendersStorage
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from core.schemas.tenders import Tender as TenderSchema
+from core.models import Tender
+
+
+class TendersService(TendersStorage):
+    def __init__(self, session: AsyncSession):
+        super().__init__(session)
+
+    async def add_tenders_in_db(self, tenders: list[TenderSchema]) -> None:
+        tenders_models = [Tender(**tender.model_dump()) for tender in tenders]
+        await self.add_all(tenders_models)
