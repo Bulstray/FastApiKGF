c38d5e0675d0843cb186a81dc072bc9d82fd826c
Ramil Khakimov <bulstray@yandex.ru>
2026-05-19 17:55:46 +0300

    fix imports

--- fastapi-kgf/rest/auth/logout.py
+++ fastapi-kgf/rest/auth/logout.py
@@ -1,8 +1,7 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
-from starlette import status
-from starlette.responses import RedirectResponse
+from fastapi import APIRouter, Depends, status
+from fastapi.responses import RedirectResponse
 
 from services.auth.session_manager import delete_session
 
