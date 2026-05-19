e8b6a4d68f867024e64ef4f9e4add5b4e10daa62
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-19 15:45:01 +0300

    expire_on_commit=False

--- fastapi-kgf/core/models/db_helper.py
+++ fastapi-kgf/core/models/db_helper.py
@@ -18,6 +18,7 @@ class DatabaseHelper:
 
         self.session_factory = async_sessionmaker(
             bind=self.engine,
+            expire_on_commit=False,
         )
 
     async def dispose(self) -> None:
