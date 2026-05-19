04e2b42cacb1f3d3e9c1a578d4f40ac06176c5f8
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-19 15:42:54 +0300

    include settings router

--- fastapi-kgf/rest/user/__init__.py
+++ fastapi-kgf/rest/user/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from .profile import router as profile_router
+from .settings import router as settings_router
 
 router = APIRouter(prefix="/users")
 
 router.include_router(profile_router)
+router.include_router(settings_router)
