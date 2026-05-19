83bf86901fb778ffaa46dd6e6513ba1fb4999547
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-19 15:47:09 +0300

    new endpoint for settings

---
+++ fastapi-kgf/rest/user/settings.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, Depends, Request
+
+
+from fastapi.responses import HTMLResponse
+from typing import Annotated
+from core.schemas import UserRead
+from dependencies.session_auth import get_current_user
+from dependencies.projects import get_project_service
+from services.projects.service import ProjectService
+from templating.jinja_template import templates
+
+router = APIRouter(prefix="/settings")
+
+
+@router.get("/", name="settings:page")
+async def settings_page(
+    request: Request,
+    current_user: Annotated[
+        UserRead,
+        Depends(get_current_user),
+    ],
+    project_service: Annotated[
+        ProjectService,
+        Depends(get_project_service),
+    ],
+) -> HTMLResponse:
+    projects = await project_service.get_all()
+
+    return templates.TemplateResponse(
+        "settings.html",
+        context={
+            "user": current_user,
+            "projects": projects,
+            "request": request,
+        },
+    )
