2f0eada0723fe50928ba471b3fa1991cbf7a8caf
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-19 15:47:32 +0300

    remove username and black

--- fastapi-kgf/core/config/superuser.py
+++ fastapi-kgf/core/config/superuser.py
@@ -5,11 +5,13 @@ from pydantic import AfterValidator, BaseModel, EmailStr
 from core.types import UserRole
 from utils import hash_password
 
-PasswordBytes = Annotated[str, AfterValidator(hash_password)]
+PasswordBytes = Annotated[
+    str,
+    AfterValidator(hash_password),
+]
 
 
 class SuperUserConfig(BaseModel):
-    username: str
     hashed_password: PasswordBytes
     name: str
     surname: str
