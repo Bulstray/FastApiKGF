d84777d69b5f520cb54111a3cca3c067b60340ff
Ramil Khakimov <bulstray@yandex.ru>
2026-05-19 18:12:26 +0300

    add TendersService, KeyWordService, ArchiveTendersService  in service tenders package

---
+++ fastapi-kgf/services/tenders/__init__.py
@@ -0,0 +1,9 @@
+__all__ = (
+    "TendersService",
+    "KeyWordService",
+    "ArchiveTendersService",
+)
+
+from .tender_service import TendersService
+from .key_word_service import KeyWordService
+from .archive_tender_service import ArchiveTendersService
