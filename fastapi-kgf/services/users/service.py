1fd9009ede7aed8478f68507037f2d30d5b1d0b9
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-20 14:51:57 +0300

    add func for auth in user_service

--- fastapi-kgf/services/users/service.py
+++ fastapi-kgf/services/users/service.py
@@ -2,7 +2,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from storage.db.crud_user import UserStorage
 
-from core.schemas import UserUpdateForm, UserUpdate
+from core.schemas import UserUpdateForm, UserUpdate, UserLogin
+from services.auth.session_manager import create_session
+
+from fastapi import Request
+import bcrypt
 
 
 class UserService(UserStorage):
@@ -11,6 +15,20 @@ class UserService(UserStorage):
     def __init__(self, session: AsyncSession) -> None:
         """Initialize the service with a database connection and user class"""
         super().__init__(session)
+
+    async def validate_basic_auth_user(self, request: Request) -> str | None:
+        async with request.form() as user_data:
+            user = UserLogin.model_validate(user_data)
+
+        is_user = await self.get_user_by_email(user.email.lower())
+
+        if is_user and bcrypt.checkpw(
+            password=user.password.encode("utf-8"),
+            hashed_password=is_user.hashed_password.encode("utf-8"),
+        ):
+            return await create_session(is_user)
+
+        return None
 
     async def update_user_data(self, user_in: UserUpdateForm, user_id: int):
         user_data = UserUpdate(
