a20a27902d4f9e34c1814047d13d8a9cb2a8262b
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-20 14:49:20 +0300

    init settings storage

---
+++ fastapi-kgf/storage/db/crud_user_settings.py
@@ -0,0 +1,26 @@
+from .base_crud import BaseCRUD
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import update, select
+from core.models import UserSettings
+from core.schemas import UserSettings as UserSchemaSchema
+
+
+class UserSettingsStorage(BaseCRUD):
+    def __init__(self, session: AsyncSession) -> None:
+        super().__init__(session, UserSettings)
+
+    async def update_settings(self, user_id, user_settings: UserSchemaSchema):
+        stmt = (
+            update(UserSettings)
+            .where(UserSettings.user_id == user_id)
+            .values(**user_settings.model_dump())
+        )
+
+        await self.session.execute(stmt)
+        await self.session.commit()
+
+    async def get_settings_by_user(self, user_id: int) -> UserSettings | None:
+        stmt = select(UserSettings).where(UserSettings.user_id == user_id)
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
