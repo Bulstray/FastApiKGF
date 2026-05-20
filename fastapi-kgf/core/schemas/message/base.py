69767287ebcf7f097657fdbce366762f2b1f89c3
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-20 14:47:21 +0300

    create messageBase

---
+++ fastapi-kgf/core/schemas/message/base.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from .file import MessageFile
+
+
+class MessageBase(BaseModel):
+    """The basic model for messages"""
+
+    task_id: int
+    text: str
+    author: int
+    created_at: str
+    file: MessageFile | None = None
