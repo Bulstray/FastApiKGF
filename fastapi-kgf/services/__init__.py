e15d12e62681721e3e716d368a710a2a36fd01b6
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-20 14:46:17 +0300

    add UserSettingsService

--- fastapi-kgf/services/__init__.py
+++ fastapi-kgf/services/__init__.py
@@ -7,11 +7,12 @@ __all__ = (
     "MessageManager",
     "ProgramService",
     "ProjectService",
+    "UserSettingsService",
 )
 
 
 from .tenders import TendersService, ArchiveTendersService, KeyWordService
-from .users import UserService
+from .users import UserService, UserSettingsService
 from .task import TasksFilesService
 from .messages import MessageManager
 from .programs import ProgramService
