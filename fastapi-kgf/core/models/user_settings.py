cce7e1472ef4a44580b4e3284d2fab2b094ff626
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-20 14:56:41 +0300

    user settings model

---
+++ fastapi-kgf/core/models/user_settings.py
@@ -0,0 +1,39 @@
+from .base import Base
+
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, false
+
+if TYPE_CHECKING:
+    from .user import User
+
+
+class UserSettings(Base):
+    __tablename__ = 'user_settings'
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey(
+            'users.id',
+            ondelete="CASCADE",
+        ),
+        unique=True,
+    )
+
+    tender_notification: Mapped[bool] = mapped_column(
+        default=False,
+        server_default=false(),
+    )
+    task_notification: Mapped[bool] = mapped_column(
+        default=False,
+        server_default=false(),
+    )
+    message_notification: Mapped[bool] = mapped_column(
+        default=False,
+        server_default=false(),
+    )
+
+    user: Mapped["User"] = relationship(
+        "User",
+        back_populates="settings",
+    )
