e096c14b428453572e5fbe36bb6a4f784f8e864a
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-20 14:56:59 +0300

    UserSettings Basemodel

---
+++ fastapi-kgf/core/schemas/user_settings.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, field_validator
+
+
+class UserSettings(BaseModel):
+    tender_notification: bool = False
+    task_notification: bool = False
+    message_notification: bool = False
+
+    @field_validator(
+        "tender_notification",
+        "task_notification",
+        "message_notification",
+        mode="before",
+    )
+    def convert_checkbox_to_bool(cls, value):
+        if type(value) is bool:
+            return value
+
+        if isinstance(value, str):
+            return value.lower() == "on"
+
+        return False
