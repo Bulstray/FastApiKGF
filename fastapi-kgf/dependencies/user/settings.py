cd8b2d2e7116aa8be6581aa1a92079c160ed8252
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-20 15:47:54 +0300

    remove classmethod

--- fastapi-kgf/dependencies/user/settings.py
+++ fastapi-kgf/dependencies/user/settings.py
@@ -1,25 +1,18 @@
-from typing import Annotated
-
-from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.models import db_helper
 from services import UserSettingsService
+from fastapi import Depends
+
+from typing import Annotated
+from core.models import db_helper
 
 
 class UserSettingsServiceFactory(UserSettingsService):
     def __init__(
         self,
-        session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-    ) -> None:
-        super().__init__(session)
-
-    @classmethod
-    def init_user_settings_factory(
-        cls,
         session: Annotated[
             AsyncSession,
             Depends(db_helper.session_getter),
         ],
-    ):
-        return cls(session)
+    ) -> None:
+        super().__init__(session)
