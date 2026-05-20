1781d32c26ce49005ab78ca9086c9e843ba710c9
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-20 15:46:44 +0300

    add factory's

---
+++ fastapi-kgf/dependencies/__init__.py
@@ -0,0 +1,8 @@
+__all__ = (
+    "UserServiceFactory",
+    "UserSettingsServiceFactory",
+    "KeyWordFactory",
+)
+
+from .user import UserServiceFactory, UserSettingsServiceFactory
+from .key_word import KeyWordFactory
