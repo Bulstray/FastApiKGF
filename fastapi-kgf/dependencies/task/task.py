dee9b21df7206fd2e824f3d164e78c51d5854a5e
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-21 10:46:44 +0300

    create task_factory

---
+++ fastapi-kgf/dependencies/task/task.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.config import settings
+from core.models import db_helper
+from services.task.service import TasksFilesService
+
+
+class TaskFactory(TasksFilesService):
+    def __init__(
+        self,
+        session: Annotated[
+            AsyncSession,
+            Depends(db_helper.get_session),
+        ],
+    ):
+        super().__init__(
+            session,
+            settings.uploads_file_task_dir,
+        )
