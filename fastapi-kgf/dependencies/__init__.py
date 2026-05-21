8cb242ae03ddaed215f3d752ef6ab893092d6822
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-21 10:47:03 +0300

    include classes

--- fastapi-kgf/dependencies/__init__.py
+++ fastapi-kgf/dependencies/__init__.py
@@ -2,7 +2,12 @@ __all__ = (
     "UserServiceFactory",
     "UserSettingsServiceFactory",
     "KeyWordFactory",
+    "TaskFactory",
+    "ProjectFactory",
+    "TaskMessageFactory",
 )
 
 from .user import UserServiceFactory, UserSettingsServiceFactory
 from .key_word import KeyWordFactory
+from .task import TaskFactory, TaskMessageFactory
+from .project import ProjectFactory
