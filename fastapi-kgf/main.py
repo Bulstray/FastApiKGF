e3dcb49dd67f122e5ee78ebeb23d1a223b01b59c
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-21 09:55:17 +0300

    add static

--- fastapi-kgf/main.py
+++ fastapi-kgf/main.py
@@ -1,16 +1,25 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from sqladmin import Admin
 
 from admin import register_admin_views
 from admin.auth import AdminAuth
 from api import router
 from app_lifespan import lifespan
-from core.config import settings
+from core.config import settings, BASE_DIR
 from core.models import db_helper
 from rest import router as main_router
 
+STATIC_PATH = BASE_DIR / "static"
+
 app = FastAPI(lifespan=lifespan)
+
+app.mount(
+    "/static",
+    StaticFiles(directory=STATIC_PATH),
+    name="static",
+)
 
 auth_backend = AdminAuth(
     secret_key=settings.secret_key_admin.secret_key,
@@ -29,6 +38,6 @@ app.include_router(main_router)
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
-        host="0.0.0.0",
-        port=8000,
+        # host="0.0.0.0",
+        # port=8000,
     )
