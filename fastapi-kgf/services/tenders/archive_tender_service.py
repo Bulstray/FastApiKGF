6225d6fa2898d089220f6c86fa1e353d9e25a7e4
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-22 13:36:31 +0300

    remove add_tenders_in_db

--- fastapi-kgf/services/tenders/archive_tender_service.py
+++ fastapi-kgf/services/tenders/archive_tender_service.py
@@ -1,17 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.models import ArchiveTender, Tender
-from core.schemas.tenders import TenderCreate as TenderSchema
 from storage.db.crud_arhive_tenders import ArchiveTendersStorage
 
 
 class ArchiveTendersService(ArchiveTendersStorage):
-    def __init__(self, session: AsyncSession):
+    def __init__(self, session: AsyncSession) -> None:
         super().__init__(session)
-
-    async def add_tenders_in_db(self, tenders: list[Tender]) -> None:
-        tenders_models = [
-            ArchiveTender(**TenderSchema.model_validate(tender))
-            for tender in tenders
-        ]
-        await self.add_all(tenders_models)
