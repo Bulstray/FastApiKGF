666aff9ea3b92320b6cb3a002dff224a345b9b8c
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-22 13:37:41 +0300

    remove basecrud

--- fastapi-kgf/storage/db/__init__.py
+++ fastapi-kgf/storage/db/__init__.py
@@ -7,9 +7,9 @@ __all__ = (
     "TendersStorage",
     "UserSettingsStorage",
     "UserStorage",
+    "ProgramsStorage",
 )
 
-from .base_crud import BaseCRUD
 from .crud_arhive_tenders import ArchiveTendersStorage
 from .crud_keyword_tenders import KeyWordTendersStorage
 from .crud_message import MessageStorage
@@ -18,3 +18,4 @@ from .crud_tasks import TaskStorage
 from .crud_tenders import TendersStorage
 from .crud_user import UserStorage
 from .crud_user_settings import UserSettingsStorage
+from .crud_programs import ProgramsStorage
