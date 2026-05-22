d3f520559ee25289bb31be1fe6b29c61d8614d1e
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-22 13:40:06 +0300

    change methods for class

--- fastapi-kgf/storage/db/crud_programs.py
+++ fastapi-kgf/storage/db/crud_programs.py
@@ -1,38 +1,10 @@
 from sqlalchemy import delete, func, select
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import Program
+from .base_crud import BaseCRUD
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
-async def get_all_programs(session: AsyncSession) -> list[Program]:
-    """Возвращает все программы как объекты Program."""
-    stmt = select(Program).order_by(Program.id)
-    result = await session.scalars(stmt)
-    return list(result.all())
-
-
-async def get_program_by_name(
-    session: AsyncSession,
-    name: str,
-) -> Program | None:
-    stmt = select(Program).where(
-        func.lower(Program.name) == name.lower(),
-    )
-    result = await session.scalars(stmt)
-    return result.first()
-
-
-async def create_program_in_db(
-    session: AsyncSession,
-    program: Program,
-) -> Program:
-    session.add(program)
-    await session.commit()
-    await session.refresh(program)
-    return program
-
-
-async def delete_program_from_db(session: AsyncSession, name: str) -> None:
-    stmt = delete(Program).where(func.lower(Program.name) == name.lower())
-    await session.execute(stmt)
-    await session.commit()
+class ProgramsStorage(BaseCRUD):
+    def __init__(self, session: AsyncSession):
+        super().__init__(session, Program)
