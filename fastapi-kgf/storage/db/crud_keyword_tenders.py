f224c264781af8646fa1729c5b3d5e0f82b20cae
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-22 13:39:15 +0300

    add anno

--- fastapi-kgf/storage/db/crud_keyword_tenders.py
+++ fastapi-kgf/storage/db/crud_keyword_tenders.py
@@ -6,5 +6,5 @@ from .base_crud import BaseCRUD
 
 
 class KeyWordTendersStorage(BaseCRUD):
-    def __init__(self, session: AsyncSession):
+    def __init__(self, session: AsyncSession) -> None:
         super().__init__(session, model=ParsingKeyword)
