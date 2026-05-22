a44c9843b9cf2ebf6686d4316438c8de82695a3b
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-22 13:31:30 +0300

    add programAdmin in admin package

--- fastapi-kgf/admin/__init__.py
+++ fastapi-kgf/admin/__init__.py
@@ -3,9 +3,11 @@ from sqladmin import Admin
 from .parser_keyword import ParserKeywordAdmin
 from .project import ProjectAdmin
 from .user import UserAdmin
+from .programs_admin import ProgramAdmin
 
 
 def register_admin_views(admin: Admin) -> None:
     admin.add_view(UserAdmin)
     admin.add_view(ProjectAdmin)
     admin.add_view(ParserKeywordAdmin)
+    admin.add_view(ProgramAdmin)
