318ce9be2d647d9f168a336944cb0a32c1826109
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-26 14:59:26 +0300

    add log

--- fastapi-kgf/tasks/new_tender_notification.py
+++ fastapi-kgf/tasks/new_tender_notification.py
@@ -1,8 +1,8 @@
-from malling.send_new_tender import send_new_tenders
-from core.schemas import TenderCreate
-from core import broker
-
 import logging
+
+from core import broker
+from core.schemas import TenderCreate
+from malling.send_new_tender import send_new_tenders
 
 log = logging.getLogger(__name__)
 
@@ -12,5 +12,5 @@ async def send_new_tenders_email(
     recipient: str,
     subject: list[TenderCreate],
 ) -> None:
-    log.info(f"Sending new tenders to %s", recipient)
+    log.info("Sending new tenders to %s", recipient)
     await send_new_tenders(recipient, subject)
