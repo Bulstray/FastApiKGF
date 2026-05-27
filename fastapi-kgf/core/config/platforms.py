dc9a1992bce0511adf3ccdb9b785b0b2d48d2168
Ramil Khakimov <bulstray@yandex.ru>
2026-05-27 19:02:01 +0300

    add anno

--- fastapi-kgf/core/config/platforms.py
+++ fastapi-kgf/core/config/platforms.py
@@ -18,9 +18,9 @@ class PlatformConfig(BaseModel):
     )
 
     @property
-    def tek_torg(self):
+    def tek_torg(self) -> str:
         return f"{self.base_platform.base_tek_torg}{self.platform_prefix_procedures.tek_torg}"
 
     @property
-    def etp_gpb(self):
+    def etp_gpb(self) -> str:
         return f"{self.base_platform.base_etp_gpb}{self.platform_prefix_procedures.etp_gpb}"
