3fcfb2b34f1544e0d764db9b80669ffc5388fc1a
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-28 10:31:43 +0300

    add annotation

--- fastapi-kgf/dependencies/programs/programs.py
+++ fastapi-kgf/dependencies/programs/programs.py
@@ -14,5 +14,5 @@ class ProgramsFactory(ProgramService):
             AsyncSession,
             Depends(db_helper.session_getter),
         ],
-    ):
+    ) -> None:
         super().__init__(session)
