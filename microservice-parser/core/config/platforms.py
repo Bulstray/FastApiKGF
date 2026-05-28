7fbebdf4051d483d4ed9e513671482d06952ba37
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-28 11:12:34 +0300

    add platforms

---
+++ microservice-parser/core/config/platforms.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+
+
+class BasePlatform(BaseModel):
+    base_tek_torg: str = "https://www.tektorg.ru"
+    base_etp_gpb: str = "https://new.etpgpb.ru"
+
+
+class PlatformPrefixProcedures(BaseModel):
+    etp_gpb: str = "/procedures"
+    tek_torg: str = "/procedures"
+
+
+class PlatformConfig(BaseModel):
+    base_platform: BasePlatform = BasePlatform()
+    platform_prefix_procedures: PlatformPrefixProcedures = (
+        PlatformPrefixProcedures()
+    )
+
+    @property
+    def tek_torg(self) -> str:
+        return (
+            f"{self.base_platform.base_tek_torg}"
+            f"{self.platform_prefix_procedures.tek_torg}"
+        )
+
+    @property
+    def etp_gpb(self) -> str:
+        return (
+            f"{self.base_platform.base_etp_gpb}"
+            f"{self.platform_prefix_procedures.etp_gpb}"
+        )
