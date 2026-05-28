cc8c403f4b1ecda436125b0ec66fff7d2ed8bc29
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-28 13:38:57 +0300

    include header requests

--- microservice-parser/core/config/settings.py
+++ microservice-parser/core/config/settings.py
@@ -1,8 +1,13 @@
 from pydantic_settings import BaseSettings
 from .platforms import PlatformConfig
 from .api import ApiPrefix
+from .headers_requests import HEADERS
 
 
 class Settings(BaseSettings):
     platforms: PlatformConfig = PlatformConfig()
     api: ApiPrefix = ApiPrefix()
+    header_requests: dict[str, str] = HEADERS
+
+
+settings = Settings()
