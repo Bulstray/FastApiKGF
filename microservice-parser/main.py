847be19ee67f2861e2a61f906bd03b2e28aff0b8
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-28 13:38:42 +0300

    include main router

--- microservice-parser/main.py
+++ microservice-parser/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 
+from api import router
+
 import uvicorn
 
 app = FastAPI(
     title="Fastapi parse tenders",
 )
+
+app.include_router(router)
 
 if __name__ == "__main__":
     uvicorn.run(
