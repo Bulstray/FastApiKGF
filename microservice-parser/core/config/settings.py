4971c742192e9d188d05f45549f90e6cdebd0523
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-28 11:43:29 +0300

    include prefix

--- microservice-parser/core/config/settings.py
+++ microservice-parser/core/config/settings.py
@@ -1,6 +1,8 @@
 from pydantic_settings import BaseSettings
 from .platforms import PlatformConfig
+from .api import ApiPrefix
 
 
 class Settings(BaseSettings):
     platforms: PlatformConfig = PlatformConfig()
+    api: ApiPrefix = ApiPrefix()
