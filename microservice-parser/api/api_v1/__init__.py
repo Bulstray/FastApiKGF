89b4dee26bc0a3e7d7440449203e8114a53953b1
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-28 13:36:04 +0300

    include tenders_router

--- microservice-parser/api/api_v1/__init__.py
+++ microservice-parser/api/api_v1/__init__.py
@@ -1,3 +1,8 @@
 from fastapi import APIRouter
 
-router = APIRouter()
+from core.config import settings
+from .tenders import router as tenders_router
+
+router = APIRouter(prefix=settings.api.v1.prefix)
+
+router.include_router(tenders_router)
