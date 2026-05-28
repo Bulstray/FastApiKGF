aaf72b91be5947ad4738e35161245aa2bf460c01
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-28 10:33:28 +0300

    wrap to AmqpDsn

--- fastapi-kgf/core/config/taskiq_config.py
+++ fastapi-kgf/core/config/taskiq_config.py
@@ -2,4 +2,4 @@ from pydantic import AmqpDsn, BaseModel
 
 
 class TaskiqConfig(BaseModel):
-    url: AmqpDsn = "amqp://guest:guest@localhost:5672//"
+    url: AmqpDsn = AmqpDsn("amqp://guest:guest@localhost:5672//")
