d28b90c20e7819374059f4109da97280cd42ea6d
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-28 10:28:00 +0300

    add cast for rar_file

--- fastapi-kgf/rest/tasks/create/create_task_endpoints.py
+++ fastapi-kgf/rest/tasks/create/create_task_endpoints.py
@@ -1,10 +1,13 @@
-from typing import Annotated
+from typing import Annotated, cast, TYPE_CHECKING
 
 from fastapi import APIRouter, Depends, Request, status
 from fastapi.responses import RedirectResponse
 
 from dependencies import ProjectFactory, TaskFactory
 from services import ProjectService
+
+if TYPE_CHECKING:
+    from fastapi import UploadFile
 
 router = APIRouter()
 
@@ -27,7 +30,8 @@ async def create_task(
         return "/"
 
     async with request.form() as form:
-        content = await form.get("rar_file").read()
+        file = cast("UploadFile", form.get("rar_file"))
+        content = await file.read()
 
         await service.create_task(
             form=form,
