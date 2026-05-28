2f064f6977f17b22e91f295252efe8d16ce5ac5b
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-28 13:39:21 +0300

    add endpoint

--- microservice-parser/api/api_v1/tenders.py
+++ microservice-parser/api/api_v1/tenders.py
@@ -1,3 +1,10 @@
 from fastapi import APIRouter
 
-router = APIRouter()
+from core.config import settings
+
+router = APIRouter(prefix=settings.api.v1.tenders)
+
+
+@router.get("/{keyword}/{keyword_id}")
+async def get_active_tenders_for_key_word(keyword: str, keyword_id: int):
+    return {"message": "Hello World"}
