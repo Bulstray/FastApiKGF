566cd06da4a48e546b1575807ef149810849d99d
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-28 15:22:34 +0300

    call parse tenders

--- microservice-parser/api/api_v1/tenders.py
+++ microservice-parser/api/api_v1/tenders.py
@@ -1,10 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from typing import Annotated
 
 from core.config import settings
+from parse.core import TenderParseCore
 
 router = APIRouter(prefix=settings.api.v1.tenders)
 
 
 @router.get("/{keyword}/{keyword_id}")
-async def get_active_tenders_for_key_word(keyword: str, keyword_id: int):
-    return {"message": "Hello World"}
+async def get_active_tenders_for_key_word(
+    parse_core: Annotated[TenderParseCore, Depends(TenderParseCore)],
+):
+    return parse_core.search_all_platforms()
