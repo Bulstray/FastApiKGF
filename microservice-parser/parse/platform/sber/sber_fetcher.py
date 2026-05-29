b55f6bcddb9928c6b055fa7b3ed3c521f018f5f6
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-29 12:32:05 +0300

    create sber fetcher

---
+++ microservice-parser/parse/platform/sber/sber_fetcher.py
@@ -0,0 +1,53 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+import time
+
+
+def page_fetcher(keyword: str) -> str:
+
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+
+    driver.get('https://www.sberbank-ast.ru/Default.aspx')
+
+    search_box = driver.find_element(
+        By.ID,
+        "txtUnitedPurchaseSearch",
+    )
+
+    time.sleep(10)
+
+    search_box.send_keys(keyword)
+    search_box.send_keys(Keys.ENTER)
+
+    filter_button = driver.find_element(
+        By.CSS_SELECTOR,
+        "button.element-in-one-row.simple-button.orange-background",
+    )
+    filter_button.click()
+
+    time.sleep(3)
+
+    stage_event = driver.find_element(
+        By.CSS_SELECTOR, 'input.shortdict-filter-choose-button'
+    )
+    stage_event.click()
+
+    time.sleep(3)
+
+    checkbox = driver.find_element(By.ID, "2216")
+    checkbox.click()
+
+    driver.execute_script("applyModal();")
+
+    driver.execute_script("applyMainFilters();")
+
+    time.sleep(10)
+
+    source_html = driver.page_source
+
+    driver.quit()
+
+    return source_html
