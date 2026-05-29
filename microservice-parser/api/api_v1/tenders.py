35323c8b445f2da6cd6086f58c09846559b3a96e
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-29 12:32:49 +0300

    add annotation

--- microservice-parser/api/api_v1/tenders.py
+++ microservice-parser/api/api_v1/tenders.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+from datetime import datetime
 
 from typing import Annotated
 
@@ -11,5 +12,5 @@ router = APIRouter(prefix=settings.api.v1.tenders)
 @router.get("/{keyword}/{keyword_id}")
 async def get_active_tenders_for_key_word(
     parse_core: Annotated[TenderParseCore, Depends(TenderParseCore)],
-):
+) -> list[dict[str, datetime | str | None | int]]:
     return parse_core.search_all_platforms()
