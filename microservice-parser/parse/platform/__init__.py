4684ae7391627f0c9662c8fa20e0aefe18a12075
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-29 12:28:35 +0300

    include sber

--- microservice-parser/parse/platform/__init__.py
+++ microservice-parser/parse/platform/__init__.py
@@ -1,7 +1,9 @@
 __all__ = (
     "EtpgpbParser",
     "TekTorgPlatform",
+    "SberPlatform",
 )
 
-from .etp_gpb import EtpgpbParser
-from .tek_torg import TekTorgPlatform
+from .etp_gpb.etp_gpb import EtpgpbParser
+from .tek_torg.tek_torg import TekTorgPlatform
+from .sber.sber import SberPlatform
