2d0047fd4b4135b8a2d1201fce7bada21482dc72
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-29 12:32:39 +0300

    add datetime or None

--- fastapi-kgf/core/schemas/tenders.py
+++ fastapi-kgf/core/schemas/tenders.py
@@ -21,7 +21,7 @@ class TenderCreate(BaseTender):
         from_attributes=True,
     )
 
-    end_date: datetime
+    end_date: datetime | None = None
 
     @field_validator("end_date", mode="before")
     @classmethod
