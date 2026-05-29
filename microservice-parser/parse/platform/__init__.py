b804ea19e1645fbf49a952eb9832b8a8ae487fef
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-05-29 16:13:20 +0300

    sort __all__

--- microservice-parser/parse/platform/__init__.py
+++ microservice-parser/parse/platform/__init__.py
@@ -1,9 +1,9 @@
 __all__ = (
     "EtpgpbParser",
-    "TekTorgPlatform",
     "SberPlatform",
+    "TekTorgPlatform",
 )
 
 from .etp_gpb.etp_gpb import EtpgpbParser
-from .tek_torg.tek_torg import TekTorgPlatform
 from .sber.sber import SberPlatform
+from .tek_torg.tek_torg import TekTorgPlatform
