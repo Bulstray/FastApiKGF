09ebf25e2da6f5c848ab93e526b07dbf1a7f0208
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-01 13:47:11 +0300

    add lukhoilPlatform

--- microservice-parser/parse/platform/__init__.py
+++ microservice-parser/parse/platform/__init__.py
@@ -2,8 +2,10 @@ __all__ = (
     "EtpgpbParser",
     "SberPlatform",
     "TekTorgPlatform",
+    "LukhoilPlatform",
 )
 
 from .etp_gpb.etp_gpb import EtpgpbParser
 from .sber.sber import SberPlatform
 from .tek_torg.tek_torg import TekTorgPlatform
+from .lukh.lukhoil import LukhoilPlatform
