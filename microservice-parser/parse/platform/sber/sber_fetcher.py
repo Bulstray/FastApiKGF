f4b71a97ba8ae849e0d054f0e3f482f0c1ddc135
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-11 12:55:07 +0300

    update

--- microservice-parser/parse/platform/sber/sber_fetcher.py
+++ microservice-parser/parse/platform/sber/sber_fetcher.py
@@ -3,55 +3,75 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def page_fetcher(keyword: str) -> str:
-
     driver = webdriver.Chrome()
     driver.maximize_window()
 
-    driver.get("https://www.sberbank-ast.ru/Default.aspx")
+    try:
 
-    search_box = driver.find_element(
-        By.ID,
-        "txtUnitedPurchaseSearch",
-    )
+        driver.get("https://www.sberbank-ast.ru/Default.aspx")
 
-    time.sleep(10)
+        search_box = driver.find_element(
+            By.ID,
+            "txtUnitedPurchaseSearch",
+        )
 
-    search_box.send_keys(keyword)
-    search_box.send_keys(Keys.ENTER)
+        time.sleep(10)
 
-    time.sleep(10)
+        search_box.send_keys(keyword)
+        search_box.send_keys(Keys.ENTER)
 
-    filter_button = driver.find_element(
-        By.CSS_SELECTOR,
-        "button.element-in-one-row.simple-button.orange-background",
-    )
-    filter_button.click()
+        time.sleep(10)
 
-    time.sleep(3)
+        filter_button = driver.find_element(
+            By.CSS_SELECTOR,
+            "button.element-in-one-row.simple-button.orange-background",
+        )
+        filter_button.click()
 
-    stage_event = driver.find_element(
-        By.CSS_SELECTOR, "input.shortdict-filter-choose-button",
-    )
-    stage_event.click()
+        time.sleep(3)
 
-    time.sleep(3)
+        stage_event = driver.find_element(
+            By.CSS_SELECTOR,
+            "input.shortdict-filter-choose-button",
+        )
+        stage_event.click()
 
-    checkbox = driver.find_element(
-        By.XPATH, "//tr[contains(., 'Подача заявок')]//input[@type='checkbox']",
-    )
-    checkbox.click()
+        time.sleep(3)
 
-    driver.execute_script("applyModal();")
+        checkbox = driver.find_element(
+            By.XPATH,
+            "//tr[contains(., 'Подача заявок')]//input[@type='checkbox']",
+        )
+        checkbox.click()
 
-    driver.execute_script("applyMainFilters();")
+        driver.execute_script("applyModal();")
 
-    time.sleep(10)
+        driver.execute_script("applyMainFilters();")
 
-    source_html = driver.page_source
+        time.sleep(10)
 
-    driver.quit()
+        source_html = driver.page_source
 
-    return source_html
+        driver.quit()
+
+        return source_html
+
+    except NoSuchElementException as error:
+        logger.error(
+            ("Ошибка при поиске элемента на странице: %s", str(error))
+        )
+        logger.debug("Детали ошибки:", exc_info=True)
+
+    except Exception as error:
+        logger.error(("Ошибка: %s", str(error)))
+        logger.debug("Детали ошибки:", exc_info=True)
+
+    finally:
+        driver.quit()
