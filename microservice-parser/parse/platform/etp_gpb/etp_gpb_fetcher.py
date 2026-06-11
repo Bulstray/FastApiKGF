92d6ca857b3ea031476e1c16d4a16b20c552bce6
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-11 12:54:47 +0300

    update

--- microservice-parser/parse/platform/etp_gpb/etp_gpb_fetcher.py
+++ microservice-parser/parse/platform/etp_gpb/etp_gpb_fetcher.py
@@ -1,13 +1,26 @@
 import time
 from urllib.parse import urlencode
+from core.config import settings
 
 from selenium import webdriver
 
 
-def page_fetcher(url: str, params: dict[str, str | int]) -> str:
+def get_params(key_word: str) -> dict[str, str | int]:
+    return {
+        "page": 1,
+        "per": 100,
+        "procedure[stage][0]": "accepting",
+        "search": key_word,
+        "sort": "by_relevance",
+    }
+
+
+def page_fetcher(key_word: str) -> str:
     driver = webdriver.Chrome()
-    driver.get(f"{url}?{urlencode(params)}")
-    time.sleep(20)
+    driver.get(
+        f"{settings.platforms.etp_gpb}?{urlencode(get_params(key_word))}"
+    )
+    time.sleep(15)
     html_source = driver.page_source
     driver.quit()
 
