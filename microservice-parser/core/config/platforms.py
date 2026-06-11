a762128991597739a8b1425fd01eedd5e4735d6d
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-11 10:45:25 +0300

    create base_zakipki_gov url

--- microservice-parser/core/config/platforms.py
+++ microservice-parser/core/config/platforms.py
@@ -8,11 +8,15 @@ class BasePlatform(BaseModel):
     base_lukh: str = (
         "https://lukoil.ru/Company/Tendersandauctions/Tenders/TendersofLukoilgroup"
     )
+    base_zakipki_gov: str = (
+        "https://zakupki.gov.ru/epz/order/extendedsearch/results.html?"
+    )
 
 
 class PlatformPrefixProcedures(BaseModel):
     etp_gpb: str = "/procedures"
     tek_torg: str = "/procedures"
+    zakipki_gov_search: str = "/epz/order/extendedsearch/results.html?"
 
 
 class PlatformConfig(BaseModel):
@@ -20,6 +24,13 @@ class PlatformConfig(BaseModel):
     platform_prefix_procedures: PlatformPrefixProcedures = (
         PlatformPrefixProcedures()
     )
+
+    @property
+    def zakupki_gov_search(self) -> str:
+        return (
+            f"{self.base_platform.base_zakipki_gov}"
+            f"{self.platform_prefix_procedures.zakipki_gov_search}"
+        )
 
     @property
     def tek_torg(self) -> str:
