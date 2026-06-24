52ece6b6c071c885c189a6b3ccaaef5be7129965
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-24 14:33:48 +0300

    remove class

--- fastapi-kgf/services/tenders/archive_tender_service.py
+++ fastapi-kgf/services/tenders/archive_tender_service.py
@@ -1,8 +1,1 @@
-from sqlalchemy.ext.asyncio import AsyncSession
 
-from storage.db.crud_arhive_tenders import ArchiveTendersStorage
-
-
-class ArchiveTendersService(ArchiveTendersStorage):
-    def __init__(self, session: AsyncSession) -> None:
-        super().__init__(session)
