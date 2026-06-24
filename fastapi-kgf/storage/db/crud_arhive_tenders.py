6a0f0b80a8425e2ced156bb0776546a969f7d3ea
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-24 14:34:01 +0300

    remove class

--- fastapi-kgf/storage/db/crud_arhive_tenders.py
+++ fastapi-kgf/storage/db/crud_arhive_tenders.py
@@ -1,20 +1,36 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
 from core.models import ArchiveTender, Tender
 from core.schemas.tenders import TenderCreate
 
-from .base_crud import BaseCRUD
+
+async def add_all_from_active_tender(
+    self,
+    session: AsyncSession,
+    tenders: list[Tender],
+) -> None:
+    archive_tenders = [
+        ArchiveTender(**TenderCreate.model_validate(tender).model_dump())
+        for tender in tenders
+    ]
+
+    session.add_all(archive_tenders)
+    await self.session.commit()
 
 
-class ArchiveTendersStorage(BaseCRUD):
-    def __init__(self, session: AsyncSession) -> None:
-        super().__init__(session, ArchiveTender)
+async def delete_archive_tender(
+    session: AsyncSession,
+    archive_tender: ArchiveTender,
+) -> None:
+    await session.delete(archive_tender)
+    await session.commit()
 
-    async def add_all_from_active_tender(self, tenders: list[Tender]) -> None:
-        archive_tenders = [
-            ArchiveTender(**TenderCreate.model_validate(tender).model_dump())
-            for tender in tenders
-        ]
 
-        self.session.add_all(archive_tenders)
-        await self.session.commit()
+async def get_archive_tender_by_url(
+    session: AsyncSession,
+    url: str,
+) -> ArchiveTender | None:
+    stmt = select(ArchiveTender).where(ArchiveTender.url == url)
+    result = await session.scalars(stmt)
+    return result.first()
