b2d6b2fda728721279be679a4acdf94d7cdd8f52
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-24 12:05:55 +0300

    remove

--- fastapi-kgf/dependencies/__init__.py
+++ fastapi-kgf/dependencies/__init__.py
@@ -1,13 +1,9 @@
 __all__ = (
-    "KeyWordFactory",
-    "ProjectFactory",
     "TaskFactory",
     "TaskMessageFactory",
     "UserServiceFactory",
     "UserSettingsServiceFactory",
 )
 
-from .key_word import KeyWordFactory
-from .project import ProjectFactory
 from .task import TaskFactory, TaskMessageFactory
 from .user import UserServiceFactory, UserSettingsServiceFactory
