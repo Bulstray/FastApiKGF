59812d79b280dd42cb73f35e83378ff5aa012dfd
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-24 14:33:16 +0300

    update imports

--- fastapi-kgf/services/tenders/__init__.py
+++ fastapi-kgf/services/tenders/__init__.py
@@ -1,9 +1,3 @@
-__all__ = (
-    "ArchiveTendersService",
-    "KeyWordService",
-    "TendersService",
-)
+__all__ = ("KeyWordService",)
 
-from .archive_tender_service import ArchiveTendersService
 from .key_word_service import KeyWordService
-from .tender_service import TendersService
