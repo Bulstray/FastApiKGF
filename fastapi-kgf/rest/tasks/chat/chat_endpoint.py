3ea47d5bb1ecb1bbb6299fa288c82e1349cb727e
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-30 11:33:51 +0300

    update

--- fastapi-kgf/rest/tasks/chat/chat_endpoint.py
+++ fastapi-kgf/rest/tasks/chat/chat_endpoint.py
@@ -1,16 +1,12 @@
 from typing import Annotated
 
-from aiopath import AsyncPath
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.models import db_helper
-from dependencies import TaskMessageFactory
 from storage.db import crud_message
-from utils import get_file_size
 
 from core.schemas.messages import MessageRead
-from core.models import Message
+from core.models import Message, db_helper
 
 router = APIRouter()
 
@@ -22,13 +18,14 @@ router = APIRouter()
 )
 async def get_messages_by_id(
     task_id: int,
-    message_service: Annotated[
-        TaskMessageFactory,
-        Depends(TaskMessageFactory),
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
     ],
 ) -> list[Message]:
 
-    return await message_service.get_messages_for_task(
+    return await crud_message.get_messages_for_task(
+        session,
         task_id,
     )
 
@@ -37,7 +34,10 @@ async def get_messages_by_id(
 async def mark_read(
     task_id: int,
     user_id: int,
-    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
 ) -> None:
     await crud_message.update_mark_read_message(
         session=session,
