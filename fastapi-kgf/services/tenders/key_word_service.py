3f0d43833138818be41b1b6cd44650ac59ccde6d
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-30 11:29:33 +0300

    update

--- fastapi-kgf/services/tenders/key_word_service.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from storage.db.crud_keyword_tenders import KeyWordTendersStorage
-
-
-class KeyWordService(KeyWordTendersStorage):
-    def __init__(self, session: AsyncSession) -> None:
-        super().__init__(session)
