1f299671b18d5c8ef84458aa895e01f30a24f1fb
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-30 11:28:41 +0300

    update

--- fastapi-kgf/rest/auth/login/__init__.py
+++ fastapi-kgf/rest/auth/login/__init__.py
@@ -3,7 +3,9 @@ from fastapi import APIRouter
 from .login_page import router as login_page_router
 from .login_post import router as login_post_router
 
-router = APIRouter(prefix="/login")
+from core.config import settings
+
+router = APIRouter(prefix=settings.api.v1.login)
 
 router.include_router(login_page_router)
 router.include_router(login_post_router)
